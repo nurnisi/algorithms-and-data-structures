93f9f32893201f9b7227cc054f3153334392dc88
nurnisi <amir.nursultan@yandex.ru>
2016-08-21 19:48:39 +0600

    mergeArrays

--- 015-mergeArrays.java
+++ 015-mergeArrays.java
@@ -1,4 +1,6 @@
-//by me
+//by me doesn't work in case:
+//    int[] a = {4,5,6,0,0,0};
+//    int[] b = {1,2,3};
 public static int[] mergeArrays(int[] arr1, int[] arr2) {
     int i = 0, j = 0;
     while(i < arr1.length && j < arr2.length) {
@@ -16,4 +18,24 @@ public static int[] mergeArrays(int[] arr1, int[] arr2) {
     }
 
     return arr1;
+}
+//by sam
+public void mergeArrays(int[] a, int[] b) {
+  if(b.length > a.length) throw new IllegalArgumentsException();
+
+  int aIndex = a.legnth - b.length - 1;
+  int bIndex = b.length - 1;
+  int mergeIndex = a.length - 1;
+
+  while(aIndex >= 0 && bIndex >= 0) {
+    if(a[aIndex] > b[bIndex]) {
+      a[mergeIndex--] = a[aIndex--];
+    } else {
+      a[mergeIndex--] = b[bIndex--];
+    }
+  }
+
+  while(bIndex >= 0) {
+    a[mergeIndex--] = b[bIndex--];
+  }
 }
