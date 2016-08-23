4987501883efe21b05238af727759986880eef75
nurnisi <amir.nursultan@yandex.ru>
2016-08-23 13:28:13 +0600

    stack via queue

--- 018-stackViaQueue.java
+++ 018-stackViaQueue.java
@@ -18,3 +18,52 @@ public class stackViaQueue {
     return stack.remove;
   }
 }
+//by sam
+public class Stack {
+  private Queue<Integer> primary = new LinkedList<Integer>();
+  private Queue<Integer> secondary = new LinkedList<Integer>();
+
+  public Stack() {}
+
+  public void push(int x) {
+    secondary.add(x);
+    while(!primary.isEmpty()) {
+      secondary.add(primary.remove);
+    }
+
+    Queue<Integer> temp = primary;
+    primary = secondary;
+    secondary = temp;
+  }
+
+  public int pop() {
+    if(primary.isEmpty()) throw new NullPointerException();
+    return primary.remove();
+  }
+}
+//another solution
+public class Stack {
+  private Queue<Integer> primary = new LinkedList<Integer>();
+  private Queue<Integer> secondary = new LinkedList<Integer>();
+
+  public Stack() {}
+
+  public void push(int x) {
+    primary.add(x);
+  }
+
+  public int pop() {
+    if(primary.isEmpty()) throw new NullPointerException();
+    Integer value;
+    while(!primary.isEmpty) {
+      value = primary.remove();
+      if(!primary.isEmpty()) secondary.add(value);
+    }
+
+    Queue<Integer> temp = primary;
+    primary = secondary;
+    secondary = temp;
+
+    return value.intValue();
+  }
+}
