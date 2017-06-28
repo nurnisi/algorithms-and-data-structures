736b9bce864bca6ed6b5dc29d5580623faf28a7f
azat <esenov7@gmail.com>
2017-06-28 23:56:58 +0600

    28.06

---
+++ leetcode/060-reverseLinkedList.java
@@ -0,0 +1,16 @@
+public static ListNode reverseList(ListNode head) {
+    if (head == null) return head;
+    Deque<ListNode> stack = new LinkedList<>();
+    while (head != null) {
+        stack.push(head);
+        head = head.next;
+    }
+    ListNode newHead = stack.pop(), runner = newHead;
+    while (!stack.isEmpty()) {
+        runner.next = stack.pop();
+        runner = runner.next;
+    }
+    runner.next = null;
+
+    return newHead;
+}
