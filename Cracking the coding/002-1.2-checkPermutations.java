3f94c4b3acdf9b6604bc155276b17ec4ab76ef6e
nurnisi <amir.nursultan@yandex.ru>
2017-07-11 16:07:17 +0600

    checkPermutation 2

--- Cracking the coding/002-1.2-checkPermutations.java
+++ Cracking the coding/002-1.2-checkPermutations.java
@@ -12,3 +12,20 @@ public static boolean checkPermutation(String str1, String str2) {
 
     return str1.equals(str2);
 }
+
+//array
+public static boolean checkPermutation(String str1, String str2) {
+    if (str1.length() != str2.length()) return false;
+
+    int[] arr = new int[128];
+    for (int i = 0; i < str1.length(); i++) {
+        arr[str1.charAt(i)]++;
+        arr[str2.charAt(i)]--;
+    }
+
+    for (int j : arr)
+        if (j != 0)
+            return false;
+
+    return true;
+}
