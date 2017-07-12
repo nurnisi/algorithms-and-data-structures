e2e2a96a42fc178df8a610cf10a9eda8d2cd805a
nurnisi <amir.nursultan@yandex.ru>
2017-07-12 16:08:32 +0600

    rotateMatrix

--- Cracking the coding/007-1.7-rotateMatrix.java
+++ Cracking the coding/007-1.7-rotateMatrix.java
@@ -6,3 +6,21 @@ public static int[][] rotateMatrix(int[][] matrix) {
             rotate[j][k] = matrix[i][j];
     return rotate;
 }
+
+//in place from ctci
+public static int[][] rotateMatrix(int[][] matrix) {
+    int n = matrix.length;
+    for (int layer = 0; layer < n / 2; layer++) {
+        int first = layer;
+        int last = n - 1 - layer;
+        for (int i = first; i < last; i++) {
+            int offset = i - first;
+            int top = matrix[first][i];
+            matrix[first][i] = matrix[last - offset][first];
+            matrix[last - offset][first] = matrix[last][last - offset];
+            matrix[last][last - offset] = matrix[i][last];
+            matrix[i][last] = top;
+        }
+    }
+    return matrix;
+}
