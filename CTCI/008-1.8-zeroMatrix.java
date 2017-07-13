c584c332de7a8527f864d45ebbf931ceeaaf22dd
nurnisi <amir.nursultan@yandex.ru>
2017-07-13 23:43:30 +0600

    zeroMatrixSpaceEfficient

--- CTCI/008-1.8-zeroMatrix.java
+++ CTCI/008-1.8-zeroMatrix.java
@@ -17,3 +17,48 @@ public static int[][] zeroMatrix(int[][] matrix) {
 
     return matrix;
 }
+
+public static int[][] zeroMatrixSpaceEfficient(int[][] matrix) {
+    if (matrix.length == 0 || matrix[0].length == 0) return matrix;
+
+    boolean rowHasZero = false, colHasZero = false;
+    for (int i = 0; i < matrix[0].length; i++)
+        if (matrix[0][i] == 0) {
+            rowHasZero = true;
+            break;
+        }
+
+    for (int i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] == 0) {
+            colHasZero = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < matrix.length; i++)
+        for (int j = 0; j < matrix[0].length; j++)
+            if (matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+
+    for (int i = 1; i < matrix[0].length; i++)
+        if (matrix[0][i] == 0)
+            for (int j = 1; j < matrix.length; j++)
+                matrix[j][i] = 0;
+
+    for (int i = 1; i < matrix.length; i++)
+        if (matrix[i][0] == 0)
+            for (int j = 1; j < matrix[0].length; j++)
+                matrix[i][j] = 0;
+
+    if (rowHasZero)
+        for (int i = 0; i < matrix[0].length; i++)
+            matrix[0][i] = 0;
+
+    if (colHasZero)
+        for (int i = 0; i < matrix.length; i++)
+            matrix[i][0] = 0;
+
+    return matrix;
+}
