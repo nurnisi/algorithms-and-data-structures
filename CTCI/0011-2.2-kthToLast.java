71dcdcbeabdf37432ba0287400d2cadff7167af2
nurnisi <amir.nursultan@yandex.ru>
2017-07-18 13:12:31 +0600

    kthToLastRecursive

--- CTCI/0011-2.2-kthToLast.java
+++ CTCI/0011-2.2-kthToLast.java
@@ -15,3 +15,15 @@ public static ListNode kthToLast(ListNode head, int k) {
 
     return current;
 }
+
+//recursive
+static int index = 0;
+public static ListNode kthToLastRecursive(ListNode head, int k) {
+    if (head == null) return head;
+
+    ListNode n = kthToLastRecursive(head.next, k);
+
+    index += 1;
+    if(index == k) return head;
+    return n;
+}
