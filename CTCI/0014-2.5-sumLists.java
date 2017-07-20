b8d9da92a994a3485e3b6c6fab48a81aacd7db08
nurnisi <amir.nursultan@yandex.ru>
2017-07-20 13:42:09 +0600

    sumListsForward recursive

--- CTCI/0014-2.5-sumLists.java
+++ CTCI/0014-2.5-sumLists.java
@@ -42,3 +42,43 @@ public static ListNode sumListsRecursive(ListNode list1, ListNode list2, int c) 
 
     return res;
 }
+
+//forward order: recursive
+public static ListNode sumListsForward(ListNode list1, ListNode list2) {
+    int i1 = 0, i2 = 0;
+    ListNode temp1 = list1, temp2 = list2;
+    while (temp1 != null) {
+        i1++;
+        temp1 = temp1.next;
+    }
+
+    while (temp2 != null) {
+        i2++;
+        temp2 = temp2.next;
+    }
+
+    return sumListsForward(list1, list2, i1, i2);
+}
+
+static int carry = 0;
+public static ListNode sumListsForward(ListNode list1, ListNode list2, int i1, int i2) {
+    if (list1 == null && list2 == null) return null;
+
+    ListNode n;
+    int sum;
+    if (i1 > i2) {
+        n = sumListsForward(list1.next, list2, --i1, i2);
+        sum = list1.val + carry;
+    } else if (i1 < i2) {
+        n = sumListsForward(list1, list2.next, i1, --i2);
+        sum = list2.val + carry;
+    } else {
+        n = sumListsForward(list1.next, list2.next, --i1, --i2);
+        sum = list1.val + list2.val + carry;
+    }
+
+    carry = sum / 10;
+    ListNode head = new ListNode(sum % 10);
+    head.next = n;
+    return head;
+}
