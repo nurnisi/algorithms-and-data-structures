5d912a7792092e9daa25433eabec6c8ec2a1ae26
nurnisi <amir.nursultan@yandex.ru>
2017-07-21 12:40:21 +0600

    isPalindromeReverse CTCI

--- CTCI/0015-2.6-isPalindrome.java
+++ CTCI/0015-2.6-isPalindrome.java
@@ -13,3 +13,30 @@ public static boolean isPalindrome(ListNode list) {
 
     return true;
 }
+
+//reverse ctci solution
+//reverse
+public static boolean isPalindromeReverse(ListNode head) {
+    ListNode reversed = reverseAndClone(head);
+    return isEqual(head, reversed);
+}
+
+public static ListNode reverseAndClone(ListNode node) {
+    ListNode head = null;
+    while (node != null) {
+        ListNode n = new ListNode(node.val);
+        n.next = head;
+        head = n;
+        node = node.next;
+    }
+    return head;
+}
+
+public static boolean isEqual(ListNode one, ListNode two) {
+    while (one != null && two != null) {
+        if (one.val != two.val) return false;
+        one = one.next;
+        two = two.next;
+    }
+    return one == null && two == null;
+}
