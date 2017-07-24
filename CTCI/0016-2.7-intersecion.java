c404b5c4a80c383f33be349c07a4b38b43e7373b
nurnisi <amir.nursultan@yandex.ru>
2017-07-24 23:33:26 +0600

    intersectionWithoutBuffer

--- CTCI/0016-2.7-intersecion.java
+++ CTCI/0016-2.7-intersecion.java
@@ -10,4 +10,36 @@ public static ListNode intersection(ListNode list1, ListNode list2) {
   }
   return null;
 }
+
 //without buffer CTCI
+public static ListNode intersectionWithoutBuffer(ListNode list1, ListNode list2) {
+  if(list1 == null || list2 == null) return null;
+
+  ListNode temp1 = list1, temp2 = list2;
+  int size1 = 1, size2 = 1;
+  while (temp1.next != null) {
+    temp1 = temp.next;
+    size1++;
+  }
+  while (temp2.next != null) {
+    temp2 = temp2.next;
+    size2++;
+  }
+  if (temp1 != temp2) return null;
+
+  ListNode longer = size1 > size2 ? list1 : list2;
+  ListNode shorter = size1 > size2 ? list2 : list1;
+
+  int diff = Math.abs(size1 - size2);
+  while (diff > 0 && longer != null) {
+    longer = longer.next;
+    diff--;
+  }
+
+  while (longer != shorter) {
+    longer = longer.next;
+    shorter = shorter.next;
+  }
+
+  return longer;
+}
