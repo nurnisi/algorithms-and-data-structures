3fdca963545bb50ea2c0b6c66316f6b2c522f695
nurnisi <amir.nursultan@yandex.ru>
2017-07-27 15:58:34 +0600

    Stack

---
+++ CTCI/0018-Stack.java
@@ -0,0 +1,34 @@
+public class MyStack {
+  private static class StackNode {
+    private int data;
+    private StackNode next;
+
+    public StackNode(int data) {
+      this.data = data;
+    }
+  }
+
+  private StackNode top;
+
+  public int pop() {
+    if (top == null) throw new EmptyStackException();
+    int data = top.data;
+    top = top.next;
+    return data;
+  }
+
+  public void push(int data) {
+    StackNode newTop = new StackNode(data);
+    newTop.next = top;
+    top = newTop;
+  }
+
+  public int peek() {
+    if (top == null) throw new EmptyStackException();
+    return top.data;
+  }
+
+  public boolean isEmpty() {
+    return top == null;
+  }
+}
