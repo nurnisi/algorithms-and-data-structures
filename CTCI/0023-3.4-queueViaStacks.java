5cf33f9d32d62ec9eb7770babad3bab17eb89327
nurnisi <amir.nursultan@yandex.ru>
2017-08-12 15:24:15 +0600

    queueViaStacks: constant push() time

--- CTCI/0023-3.4-queueViaStacks.java
+++ CTCI/0023-3.4-queueViaStacks.java
@@ -1,3 +1,4 @@
+//constant pop() time
 public class MyQueue {
   Stack temp, main;
 
@@ -14,6 +15,31 @@ public class MyQueue {
 
   public int pop() {
     return main.pop();
+  }
+}
+
+//constant push() time
+public class MyQueue {
+  Stack temp, main;
+
+  public MyQueue() {
+    temp = new Stack();
+    main = new Stack();
+  }
+
+  public void push(int item) {
+    main.push(item);
+  }
+
+  public int pop() {
+    if (main.isEmpty) throw new EmptyQueueException;
+    int item;
+    while (!main.isEmpty()) {
+      item = main.pop();
+      if (!main.isEmpty()) temp.push(item);
+    }
+    while (!temp.isEmpty()) main.push(temp.pop());
+    return item;
   }
 }
 
