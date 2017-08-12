cafa6a2346706d21ed74bfe0f60f6e37cff403e6
nurnisi <amir.nursultan@yandex.ru>
2017-08-12 14:50:38 +0600

    SetOfStacks CTCI popAt()

--- CTCI/0022-3.3-setOfStacks.java
+++ CTCI/0022-3.3-setOfStacks.java
@@ -38,3 +38,102 @@ public static class SetOfStacks {
     return item;
   }
 }
+
+// CTCI popAt()
+public class SetOfStacks {
+  List<Stack> stacks = new ArrayList<>();
+  int capacity;
+
+  SetOfStacks(int capacity) {
+    this.capacity = capacity;
+  }
+
+  public Stack getLastStack() {
+    if (stacks.isEmpty()) return null;
+    return stacks.get(stacks.size() - 1);
+  }
+
+  public void push(int item) {
+    Stack last = getLastStack();
+    if (last != null && !last.isFull()) {
+      last.push(v);
+    } else {
+      Stack stack = new Stack(capacity);
+      stack.push(item);
+      stacks.add(stack);
+    }
+  }
+
+  public int pop() {
+    Stack last = getLastStack();
+    if (last == null) throw new EmptyStackException;
+    int item = last.pop();
+    if (last.size == 0) stacks.remove(stacks.size() - 1);
+    return item;
+  }
+
+  public boolean isEmpty() {
+    Stack last = getLastStack();
+    return last == null || last.isEmpty();
+  }
+
+  public int popAt(int index) {
+    return leftShift(index, true);
+  }
+
+  private int leftShift(int index, boolean removeTop) {
+    Stack stack = stacks.get(index);
+    int removedItem;
+    if (removeTop) removedItem = stack.pop();
+    else removedItem = stack.removeBottom();
+    if (stack.isEmpty()) stacks.remove(index);
+    else if (stacks.size() > index + 1) {
+      int item = leftShift(index + 1, false);
+      stack.push(item);
+    }
+    return removedItem;
+  }
+}
+
+public class Stack {
+  private int capacity;
+  public Node top, bottom;
+  public int size = 0;
+
+  public Stack(int capacity) { this.capacity = capacity; }
+  public boolean isFull() { return capacity == size; }
+
+  public void join(Node above, Node below) {
+    if (below != null) below.above = above;
+    if (above != null) above.below = below;
+  }
+
+  public boolean push(int item) {
+    if (size >= capacity) return false;
+    size++;
+    Node n = new Node(item);
+    if (size == 1) bottom = n;
+    join(n, top);
+    top = n;
+    return true;
+  }
+
+  public int pop() {
+    Node t = top;
+    top = top.below;
+    size--;
+    return t.value;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public int removeBottom() {
+    Node b = bottom;
+    bottom = bottom.above;
+    if (bottom != null) bottom.below = null;
+    size--;
+    return b.value;
+  }
+}
