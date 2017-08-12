871621858650e40153ff70d68815fddc92d5f02c
nurnisi <amir.nursultan@yandex.ru>
2017-08-12 16:59:46 +0600

    sortStack

---
+++ CTCI/0024-3.5-sortStack.java
@@ -0,0 +1,17 @@
+public static Stack sortStack(Stack stack) {
+  Stack temp = new Stack();
+  int item, counter = 0;
+  while (!stack.isEmpty()) {
+    item = stack.pop();
+    while (!temp.isEmpty() && item > temp.peek()) {
+      stack.push(temp.pop());
+      counter++;
+    }
+    temp.push(item);
+    while (counter > 0) {
+      temp.push(stack.pop());
+      counter--;
+    }
+  }
+  return temp;
+}
