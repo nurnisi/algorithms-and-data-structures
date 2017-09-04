9ae2cb03d5f41e3922bd4897b50356909ded41b5
nurnisi <amir.nursultan@yandex.ru>
2017-09-04 15:04:01 +0600

    CTCI: getNextArithmetic

--- CTCI/0029-5.4-nextNumber.java
+++ CTCI/0029-5.4-nextNumber.java
@@ -74,3 +74,21 @@ static int getPrev(int num) {
 
     return num;
 }
+
+//CTCI: 
+static int getNextArithmetic(int num) {
+    int c = num, c0 = 0, c1 = 0;
+    while ((c & 1) == 0 && c != 0) {
+        c0++;
+        c >>= 1;
+    }
+
+    while ((c & 1) == 1) {
+        c1++;
+        c >>= 1;
+    }
+
+    if (c0 + c1 == 31 || c0 + c1 == 0) return -1;
+
+    return num + (1 << c0) + (1 << (c1 - 1)) - 1;
+}
