e7ae1a1ec2dbd1c61c2d05ac01d9fd1b2187f9c5
nurnisi <amir.nursultan@yandex.ru>
2017-09-05 17:16:32 +0600

    isBalanced - 2

--- CTCI/0037-4.4-isBalanced.java
+++ CTCI/0037-4.4-isBalanced.java
@@ -12,3 +12,22 @@ int getHeight(TreeNode node) {
     if (node == null) return -1;
     return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
 }
+
+//efficient - O(N)
+boolean isBalanced(TreeNode root) {
+    return checkHeight(root) != Integer.MIN_VALUE;
+}
+
+int checkHeight(TreeNode root) {
+    if (root == null) return -1;
+
+    int left_height = checkHeight(root.left);
+    if (left_height == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+    int right_height = checkHeight(root.right);
+    if (right_height == Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+    int height_diff = Math.abs(left_height - right_height);
+    if (height_diff > 1) return Integer.MIN_VALUE;
+    else return Math.max(left_height - right_height) + 1;
+}
