85bc9338607d59fc3e5f9979a389cdaa4c612289
nurnisi <amir.nursultan@yandex.ru>
2017-09-07 12:30:03 +0600

    Solution 3: without links to parents

--- CTCI/0042-4.8-commonAncestor.java
+++ CTCI/0042-4.8-commonAncestor.java
@@ -61,3 +61,25 @@ TreeNode getSubling(TreeNode node) {
     TreeNode parent = node.parent;
     return parent.left == node ? parent.right : parent.left;
 }
+
+// Solution 3: without links to parents
+TreeNode commonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
+    if (!covers(root, node1) || !covers(root, node2)) return null;
+    return helper(root, node1, node2);
+}
+
+TreeNode helper(TreeNode root, TreeNode node1, TreeNode node2) {
+    if (root == null || root == node1 || root == node2) return root;
+
+    boolean node1IsOnLeft = covers(root.left, node1);
+    boolean node2IsOnLeft = covers(root.left, node2);
+    if (node1IsOnLeft != node2IsOnLeft) return root;
+    TreeNode childSide = node1IsOnLeft ? root.left : root.right;
+    return helper(childSide, node1, node2);
+}
+
+boolean covers(TreeNode node1, TreeNode node2) {
+    if (node1 == null) return false;
+    if (node1 == node2) return true;
+    return covers(node1.left, node2) || covers(node1.right, node2);
+}
