0e5a1544ca377193f7e6dacb0ea1135783dd7c2a
nurnisi <amir.nursultan@yandex.ru>
2017-09-07 12:07:20 +0600

    Solution 2: with links to parents but with better worst-case runtime

--- CTCI/0042-4.8-commonAncestor.java
+++ CTCI/0042-4.8-commonAncestor.java
@@ -33,3 +33,31 @@ int depth(TreeNode n) {
     }
     return depth;
 }
+
+// Solution 2: with links to parents but with better worst-case runtime
+TreeNode commonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
+    if (!covers(root, node1) || !covers(root, node2) return null;
+    if (covers(node1, node2)) return node1;
+    if (covers(node2, node1)) return node2;
+
+    TreeNode subling = getSubling(node1);
+    TreeNode parent = node1.parent;
+    while(!covers(subling, node2)) {
+        subling = getSubling(parent);
+        parent = parent.parent;
+    }
+
+    return parent;
+}
+
+boolean covers(TreeNode node1, TreeNode node2) {
+    if (node1 == null) return false;
+    if (node1 == node2) return true;
+    return covers(node1.left, node2) || covers(node1.right, node2);
+}
+
+TreeNode getSubling(TreeNode node) {
+    if (node == null || node.parent == null) return null;
+    TreeNode parent = node.parent;
+    return parent.left == node ? parent.right : parent.left;
+}
