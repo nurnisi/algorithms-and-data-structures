6c5e565cac70a49b03e80773a8fe4550cf631733
nurnisi <amir.nursultan@yandex.ru>
2017-09-09 00:28:21 +0600

    containsTree: Alternative approach

--- CTCI/0044-4.10-checkSubtree.java
+++ CTCI/0044-4.10-checkSubtree.java
@@ -18,3 +18,22 @@ void getOrderString(TreeNode node, StringBuilder sb) {
     getOrderString(node.left, sb);
     getOrderString(node.right, sb);
 }
+
+// Alternative approach
+boolean containsTree(TreeNode t1, TreeNode t2) {
+    if (t2 == null) return true;
+    return subTree(t1, t2);
+}
+
+boolean subTree(TreeNode t1, TreeNode t2) {
+    if (t1 == null) return false;
+    else if (t1.data == t2.data && matchTree(t1, t2)) return true;
+    return subTree(t1.left, t2) || subTree(t1.right, t2);
+}
+
+boolean matchTree(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) return true;
+    else if (t1 == null || t2 == null) return false;
+    else if (t1.data != t2.data) return false;
+    else return matchTree(t1.left, t2.left) && matchTree(t1.right, t2.right);
+}
