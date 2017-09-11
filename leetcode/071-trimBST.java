b0ad40000723ad34267a815de755ecb0a4fd828c
nurnisi <amir.nursultan@yandex.ru>
2017-09-11 17:17:25 +0600

    leetcode: trimBST

--- leetcode/071-trimBST.java
+++ leetcode/071-trimBST.java
@@ -29,3 +29,14 @@ public void insert(TreeNode node, int value) {
         else insert(node.right, value);
     }
 }
+
+// leetcode
+public TreeNode trimBST(TreeNode root, int L, int R) {
+    if (root == null) return null;
+
+    if (root.val > R) return trimBST(root.left, L, R);
+    if (root.val < L) return trimBST(root.right, L, R);
+
+    root.left = trimBST(root.left, L, R);
+    root.right = trimBST(root.right, L, R);
+}
