f83971a47bff01182da2cc399cc9e6a7f9b89bfd
nurnisi <amir.nursultan@yandex.ru>
2017-09-12 00:51:28 +0600

    71A - Way Too Long Words

---
+++ codeforces/0001 - 71A - Way Too Long Words.java
@@ -0,0 +1,10 @@
+static Scanner sc = new Scanner(System.in);
+public static void main(String[] args) {
+    int i = sc.nextInt();
+    for (; i >= 0; i--) {
+        String word = sc.nextLine();
+        int length = word.length();
+        if (length > 10) word = String.format("%s%s%s", word.charAt(0), length - 2, word.charAt(length - 1));
+        System.out.println(word);
+    }
+}
