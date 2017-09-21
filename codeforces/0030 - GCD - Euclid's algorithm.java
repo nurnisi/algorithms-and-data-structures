98cd7d88bbd23dc80ab19f40f662b061e9cc03d4
nurnisi <amir.nursultan@yandex.ru>
2017-09-21 11:18:42 +0600

    non-recursive

--- codeforces/0030 - GCD - Euclid's algorithm.java
+++ codeforces/0030 - GCD - Euclid's algorithm.java
@@ -5,3 +5,12 @@ static int gcd(int a, int b) {
     return gcd(b, a % b);
 }
 
+// non-recursive
+static int gcd(int a, int b) {
+    while (b > 0) {
+        int temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
+}
