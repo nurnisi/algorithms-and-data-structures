6d3f8e0012492162dfb60bce1c0b955085bc4e77
nurnisi <amir.nursultan@yandex.ru>
2017-09-21 12:01:34 +0600

    lcm

---
+++ codeforces/0031 - LCM.java
@@ -0,0 +1,20 @@
+static int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+// recursive
+static int gcd(int a, int b) {
+    System.out.println(a + " " + b);
+    if (b == 0) return  a;
+    return gcd(b, a % b);
+}
+
+// non-recursive
+static int gcd(int a, int b) {
+    while (b > 0) {
+        int temp = a;
+        a = b;
+        b = temp % b;
+    }
+    return a;
+}
