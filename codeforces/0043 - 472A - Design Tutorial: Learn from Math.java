52a7fb58f779df1c8d459b51f00a2a776a585eb5
nurnisi <amir.nursultan@yandex.ru>
2017-09-29 12:18:59 +0600

    472A - Design Tutorial: Learn from Math

---
+++ codeforces/0043 - 472A - Design Tutorial: Learn from Math.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        designTutorial();
+    }
+
+    static boolean isComposite(int n) {
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                return true;
+        return false;
+    }
+
+    static void designTutorial() {
+        int n = sc.nextInt();
+        for (int i = 2; i * 2 <= n; i++) {
+            if (isComposite(i) && isComposite(n - i)) {
+                System.out.println(i + " " + (n - i));
+                return;
+            }
+        }
+    }
+
+}
