7c981a4c40818f822d58b18480f588fc7c8bc798
nurnisi <amir.nursultan@yandex.ru>
2017-10-02 20:33:31 +0600

    boysAndGirls

---
+++ CTCI/0047-6.7-boysAndGirls.java
@@ -0,0 +1,22 @@
+static double boysAndGirls(int families) {
+    int girls = 0;
+    int boys = 0;
+    for (int i = 0; i < n; i++) {
+        int[] gender = runOneFamily();
+        girls += gender[0];
+        boys += gender[1];
+    }
+    return girls / (double) (boys + girls);
+}
+
+static int[] runOneFamily() {
+    Random random = new Random();
+    int boys = 0;
+    int girls = 0;
+    while (girls == 0) {
+        if (random.nextBoolean()) girls++;
+        else boys++;
+    }
+    int[] gender = {girls, boys};
+    return gender;
+}
