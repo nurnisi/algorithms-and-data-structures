2220ffe9d79811b7ca89c06b65dac3677b6472cc
nurnisi <amir.nursultan@yandex.ru>
2018-02-24 13:31:59 +0600

    c1modified

--- menshikov/003_1c.java
+++ menshikov/003_1c.java
@@ -8,6 +8,37 @@ public class main {
         c1();
     }
 
+    static void c1modified() {
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] cons = new int[n];
+        int maxIndex = 0, max = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int k = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) k = Math.max(k, cons[j]);
+            }
+            cons[i] = k + 1;
+            if (max < cons[i]) {
+                max = cons[i];
+                maxIndex = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.print(arr[maxIndex] + " ");
+        for (int i = maxIndex + 1; i < n && cons[maxIndex] > 0; i++) {
+            if (cons[maxIndex] - 1 == cons[i]) {
+                System.out.print(arr[i] + " ");
+                maxIndex = i;
+            }
+        }
+    }
+
     static void c1() {
         int n = sc.nextInt();
         int[] arr = new int[n];
@@ -38,6 +69,7 @@ public class main {
             }
         }
 
+        System.out.println(max);
         while (!stack.isEmpty()) System.out.println(stack.pop());
     }
 }
