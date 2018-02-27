4fc242a5741977a45eabf73f9a5d8777b24ac565
nurnisi <amir.nursultan@yandex.ru>
2018-02-27 12:17:45 +0600

    2a 2

--- menshikov/007_2a.java
+++ menshikov/007_2a.java
@@ -5,7 +5,37 @@ public class main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        a2();
+        a2_2();
+    }
+
+    static void a2_2() {
+        List<Integer> primes = new ArrayList<>();
+        for (int i = 2; i <= 1000; i++)
+            if (isPrime(i)) primes.add(i);
+
+        int n = sc.nextInt(), m = sc.nextInt();
+
+        int sqrt = (int) Math.sqrt(n);
+        boolean globCheck = true;
+
+        while (n <= m) {
+            int i = 0;
+            boolean check = true;
+            while (primes.get(i) <= sqrt) {
+                if (n % primes.get(i) == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check) {
+                globCheck = false;
+                System.out.println(n);
+            }
+            n++;
+            if ((int) Math.pow(sqrt + 1, 2) == n) sqrt++;
+        }
+        if (globCheck) System.out.println("Absent");
     }
 
     static void a2() {
