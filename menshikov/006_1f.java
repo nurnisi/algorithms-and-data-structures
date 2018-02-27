56faf1d57af99400f5da8f5153eb59795095e53f
nurnisi <amir.nursultan@yandex.ru>
2018-02-27 10:28:23 +0600

    f1_2

--- menshikov/006_1f.java
+++ menshikov/006_1f.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class main {
@@ -6,7 +5,40 @@ public class main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        f1();
+        f1_2();
+    }
+
+    static void f1_2() {
+        int[] A = new int[5];
+        int[] B = new int[13];
+        int[] C = new int[6];
+
+        for (int i = 0; i < 5; i++) {
+            A[i] = sc.nextInt();
+            B[A[i] - 1]++;
+        }
+
+        for (int i = 0; i < 13; i++) {
+            C[B[i]]++;
+        }
+
+        if (C[5] == 1) System.out.println("Impossible");
+        else if (C[4] == 1) System.out.println("Four of a Kind");
+        else if (C[3] == 1 && C[2] == 1) System.out.println("Full House");
+        else if (C[3] == 1) System.out.println("Three of a Kind");
+        else if (C[2] == 2) System.out.println("Two Pairs");
+        else if (C[2] == 1) System.out.println("One Pair");
+        else {
+            int i = 0;
+            while (B[i] != 1) i++;
+            for (int j = 1; j < 5; j++) {
+                if (B[i + j] != 1) {
+                    System.out.println("Nothing");
+                    return;
+                }
+            }
+            System.out.println("Straight");
+        }
     }
 
     static void f1() {
