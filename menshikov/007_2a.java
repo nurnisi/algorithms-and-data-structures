3c6a875b0a5a6fc8bd51b2f2b6a6d8664867af43
nurnisi <amir.nursultan@yandex.ru>
2018-02-28 12:10:06 +0600

    2a_3

--- menshikov/007_2a.java
+++ menshikov/007_2a.java
@@ -5,7 +5,31 @@ public class main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        b2_2();
+        b2_3();
+    }
+
+    static void b2_3() {
+        String s = sc.nextLine();
+        char[] rez = new char[8];
+        boolean[] used = new boolean[8];
+
+        recursion_3(0, s, s.length(), rez, used);
+    }
+
+    static void recursion_3(int cur, String s, int len, char[] rez, boolean[] used) {
+        if (cur == len) {
+            for (int i = 0; i < len; i++) System.out.print(rez[i]);
+            System.out.println();
+        } else {
+            for (int i = 0; i < len; i++) {
+                if (!used[i]) {
+                    rez[i] = s.charAt(cur);
+                    used[i] = !used[i];
+                    recursion_3(cur + 1, s, len, rez, used);
+                    used[i] = !used[i];
+                }
+            }
+        }
     }
 
     static void b2_2() {
@@ -13,20 +37,19 @@ public class main {
         char[] rez = new char[8];
         boolean[] used = new boolean[8];
 
-        recursion(0, s, s.length(), rez, used);
+        recursion_2(0, s, s.length(), rez, used);
     }
 
-    static void recursion(int cur, String s, int len, char[] rez, boolean[] used) {
+    static void recursion_2(int cur, String s, int len, char[] rez, boolean[] used) {
         if (cur == len) {
             for (int i = 0; i < len; i++) System.out.print(rez[i]);
             System.out.println();
-        }
-        else {
+        } else {
             for (int i = 0; i < len; i++) {
                 if (!used[i]) {
                     rez[cur] = s.charAt(i);
                     used[i] = !used[i];
-                    recursion(cur + 1, s, len, rez, used);
+                    recursion_2(cur + 1, s, len, rez, used);
                     used[i] = !used[i];
                 }
             }
