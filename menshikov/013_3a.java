33ddc33acaf6de27285eb65b3173e128d6ffec06
nurnisi <amir.nursultan@yandex.ru>
2018-03-06 15:08:14 +0600

    a3_2_optimized

--- menshikov/013_3a.java
+++ menshikov/013_3a.java
@@ -8,6 +8,21 @@ public class main {
         a3();
     }
 
+    static void a3_2_optimized() {
+        int n = sc.nextInt();
+
+        int i = 2;
+        while (i * i <= n) {
+            if (n % i == 0) {
+                n /= i;
+                System.out.print(i + (n != 1 ? "*" : ""));
+            } else if (i == 2) i++;
+            else i += 2;
+        }
+
+        if (n != 1) System.out.print(n);
+    }
+
     static void a3() {
         int n = sc.nextInt();
 
