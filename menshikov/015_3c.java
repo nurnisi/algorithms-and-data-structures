e5a2e417f4b6f8ca00f0873bc4fa3ae07bfbd404
nurnisi <amir.nursultan@yandex.ru>
2018-03-28 16:18:57 +0600

    3c

---
+++ menshikov/015_3c.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        c3();
+    }
+
+    static void c3() {
+        int empty = sc.nextInt();
+        int full = sc.nextInt();
+        int n = sc.nextInt();
+
+        Map<Integer, Integer> W = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            W.put(sc.nextInt(), sc.nextInt());
+        }
+
+        int[] min = new int[full - empty + 1];
+        int[] max = new int[full - empty + 1];
+        min[0] = 0;
+        max[0] = 0;
+        for (int weight = 1; weight < min.length; weight++) {
+            boolean checkMin = true;
+            boolean checkMax = true;
+            for (int kind : W.keySet()) {
+                int wOfKind = W.get(kind);
+                if (wOfKind <= weight) {
+                    if (min[weight - wOfKind] >= 0) {
+                        if (checkMin) min[weight] = min[weight - wOfKind] + kind;
+                        else min[weight] = Math.min(min[weight], min[weight - wOfKind] + kind);
+                        checkMin = false;
+                    }
+                    if (max[weight - wOfKind] >= 0) {
+                        if (checkMax) max[weight] = max[weight - wOfKind] + kind;
+                        else max[weight] = Math.max(max[weight], max[weight - wOfKind] + kind);
+                        checkMax = false;
+                    }
+                }
+            }
+            if (checkMin) min[weight] = -1;
+            if (checkMax) max[weight] = -1;
+        }
+        if (min[min.length - 1] == -1 || max[max.length - 1] == -1) System.out.println("This is impossible");
+        else System.out.println(min[min.length - 1] + " " + max[max.length - 1]);
+    }
+}
