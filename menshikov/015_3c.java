f131061ad976ec4eb4c8ee87f3082bf2ac6fbc0c
nurnisi <amir.nursultan@yandex.ru>
2018-03-28 17:45:05 +0600

    c3_2

--- menshikov/015_3c.java
+++ menshikov/015_3c.java
@@ -8,6 +8,45 @@ public class main {
         c3();
     }
 
+    static void c3_2() {
+        int empty = sc.nextInt();
+        int full = sc.nextInt();
+        int n = sc.nextInt();
+
+        Map<Integer, Integer> W = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            W.put(sc.nextInt(), sc.nextInt());
+        }
+
+        long[] min = new long[full - empty + 1];
+        Arrays.fill(min, -1);
+        min[0] = 0;
+
+        long[] max = new long[full - empty + 1];
+        Arrays.fill(max, -1);
+        max[0] = 0;
+
+
+        for (int kind : W.keySet()) {
+            int wOfKind = W.get(kind);
+            for (int weight = 1; weight < min.length; weight++) {
+                if (wOfKind <= weight) {
+                    if (min[weight - wOfKind] >= 0) {
+                        if (min[weight] == -1) min[weight] = min[weight - wOfKind] + kind;
+                        else min[weight] = Math.min(min[weight], min[weight - wOfKind] + kind);
+                    }
+                    if (max[weight - wOfKind] >= 0) {
+                        if (max[weight] == -1) max[weight] = max[weight - wOfKind] + kind;
+                        else max[weight] = Math.max(max[weight], max[weight - wOfKind] + kind);
+                    }
+                }
+            }
+        }
+
+        if (min[min.length - 1] == -1 || max[max.length - 1] == -1) System.out.println("This is impossible.");
+        else System.out.println(min[min.length - 1] + " " + max[max.length - 1]);
+    }
+
     static void c3() {
         int empty = sc.nextInt();
         int full = sc.nextInt();
@@ -18,8 +57,8 @@ public class main {
             W.put(sc.nextInt(), sc.nextInt());
         }
 
-        int[] min = new int[full - empty + 1];
-        int[] max = new int[full - empty + 1];
+        long[] min = new long[full - empty + 1];
+        long[] max = new long[full - empty + 1];
         min[0] = 0;
         max[0] = 0;
         for (int weight = 1; weight < min.length; weight++) {
@@ -43,7 +82,7 @@ public class main {
             if (checkMin) min[weight] = -1;
             if (checkMax) max[weight] = -1;
         }
-        if (min[min.length - 1] == -1 || max[max.length - 1] == -1) System.out.println("This is impossible");
+        if (min[min.length - 1] == -1 || max[max.length - 1] == -1) System.out.println("This is impossible.");
         else System.out.println(min[min.length - 1] + " " + max[max.length - 1]);
     }
 }
