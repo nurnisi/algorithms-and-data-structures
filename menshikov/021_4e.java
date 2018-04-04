50ffbd8cdb21df1444e1f587c7810b39370fe4ca
nurnisi <amir.nursultan@yandex.ru>
2018-04-04 18:18:47 +0600

    4e_2

--- menshikov/021_4e.java
+++ menshikov/021_4e.java
@@ -5,7 +5,32 @@ public class main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        e4();
+        e4_2();
+    }
+
+    static void e4_2() {
+        String n = sc.nextLine();
+        int div = sc.nextInt();
+
+        int[] num = new int[n.length()];
+        for (int i = 0; i < n.length(); i++) num[i] = n.charAt(i) - '0';
+
+        if (num[0] == 0) {
+            System.out.println(0 + "\n" + 0);
+            return;
+        }
+
+        int rem = 0;
+        for (int i = 0; i < num.length; i++) {
+            rem = rem * 10 + num[i];
+            num[i] = rem / div;
+            rem = rem % div;
+        }
+
+        int i = 0;
+        while (num[i] == 0) i++;
+        while (i < num.length) System.out.print(num[i++]);
+        System.out.println("\n" + rem);
     }
 
     //my attempt: partial solution
@@ -15,7 +40,7 @@ public class main {
         StringBuilder div = new StringBuilder();
         int cur = s.charAt(0) - '0', i = 1;
 
-        if (s.length() <= 32) System.out.println(Integer.valueOf(s) / n + "\n" + Integer.valueOf(s) % n);
+        if (s.length() <= 18) System.out.println(Long.valueOf(s) / n + "\n" + Long.valueOf(s) % n);
         else {
             while (i < s.length()) {
                 if (cur < n) cur = cur * 10 + (s.charAt(i++) - '0');
