9404b1d73adaff6c1426a83935ae0779029a1509
nurnisi <amir.nursultan@yandex.ru>
2018-04-06 16:21:10 +0600

    4f_2

--- menshikov/024_4f.java
+++ menshikov/024_4f.java
@@ -5,7 +5,28 @@ public class main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        f4();
+        f4_2();
+    }
+
+    static void f4_2() {
+        int n = Integer.valueOf(sc.nextLine());
+        String s = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') stack.push(ch);
+            else if (stack.isEmpty()
+                    || (ch == ')' && stack.peek() != '(')
+                    || (ch == '}' && stack.peek() != '{')
+                    || (ch == ']' && stack.peek() != '[')) {
+                System.out.println("No");
+                return;
+            } else stack.pop();
+        }
+
+        if (stack.isEmpty()) System.out.println("Yes");
+        else System.out.println("No");
     }
 
     static void f4() {
