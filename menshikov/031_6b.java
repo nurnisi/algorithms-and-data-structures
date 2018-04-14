1cdeab6f984e1ff9f5715ccc0e643b466e8c21d9
nurnisi <amir.nursultan@yandex.ru>
2018-04-14 14:29:44 +0600

    6b_2

--- menshikov/031_6b.java
+++ menshikov/031_6b.java
@@ -5,7 +5,33 @@ public class main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        b6();
+        b6_2();
+    }
+
+    //solution with 2D array
+    static void b6_2() {
+        int N = sc.nextInt(), max = 0;
+        int[] inputs = new int[N];
+        for (int i = 0; i < N; i++) {
+            inputs[i] = sc.nextInt();
+            max += inputs[i];
+        }
+
+        boolean[][] B = new boolean[N + 1][max + 1];
+        B[0][0] = true;
+        for (int L = 1; L < B.length; L++) {
+            for (int S = 0; S < B[0].length; S++) {
+                B[L][S] = B[L - 1][S];
+                if (S - inputs[L - 1] >= 0) B[L][S] = B[L][S] || B[L - 1][S - inputs[L - 1]];
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < B[0].length; i++)
+            if (B[N][i])
+                res++;
+
+        System.out.println(res);
     }
 
     //my solution: does not pass time limit
