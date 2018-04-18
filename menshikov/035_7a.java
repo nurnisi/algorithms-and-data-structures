aa578b8c36f3dda2c20a0f46b2a402d23eadc32f
nurnisi <amir.nursultan@yandex.ru>
2018-04-18 17:55:39 +0600

    a7_3_stern_brokot_tree

--- menshikov/035_7a.java
+++ menshikov/035_7a.java
@@ -7,7 +7,30 @@ public class acmp {
     static Scanner sc;
 
     public static void main(String[] args) throws IOException {
-        a7_2_farey();
+        a7_3_stern_brokot_tree();
+    }
+
+    static int N;
+    static List<String> list = new ArrayList<>();
+
+    static void a7_3_stern_brokot_tree() throws IOException {
+        sc = new Scanner(new File("input.txt"));
+        pw = new PrintWriter(new File("output.txt"));
+
+        N = sc.nextInt();
+
+        rec(1, 2, 0, 1, 1, 1);
+
+        for (String s: list) pw.println(s);
+        pw.close();
+    }
+
+    static void rec(int i, int j, int lefti, int leftj, int righti, int rightj) {
+        if (j <= N) {
+            rec(i + lefti, j + leftj, lefti, leftj, i, j);
+            list.add(i + "/" + j);
+            rec(i + righti, j + rightj, i, j, righti, rightj);
+        }
     }
 
     static void a7_2_farey() throws IOException {
