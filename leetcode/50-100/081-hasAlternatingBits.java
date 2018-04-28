8410118335397d90c749ab565ba09cd82d140d8f
nurnisi <amir.nursultan@yandex.ru>
2018-04-28 16:49:41 +0600

    hasAlternatingBits3

--- leetcode/50-100/081-hasAlternatingBits.java
+++ leetcode/50-100/081-hasAlternatingBits.java
@@ -3,7 +3,15 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(hasAlternatingBits2(11));
+        System.out.println(hasAlternatingBits3(5));
+    }
+
+    public static boolean hasAlternatingBits3(int n) {
+        String bi = Integer.toBinaryString(n);
+        for (int i = 0; i < bi.length() - 1; i++)
+            if (bi.charAt(i) == bi.charAt(i + 1))
+                return false;
+        return true;
     }
 
     public static boolean hasAlternatingBits2(int n) {
