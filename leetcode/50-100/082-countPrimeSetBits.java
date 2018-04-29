a47ea25ba06d2b2e85b6109bf6cc5c4627b6bf68
nurnisi <amir.nursultan@yandex.ru>
2018-04-30 00:04:40 +0600

    countPrimeSetBits2

--- leetcode/50-100/082-countPrimeSetBits.java
+++ leetcode/50-100/082-countPrimeSetBits.java
@@ -3,7 +3,22 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(countPrimeSetBits(10, 15));
+        System.out.println(countPrimeSetBits2(6, 10));
+    }
+
+    public static int countPrimeSetBits2(int L, int R) {
+        int ans = 0;
+        for (int i = L; i <= R; i++)
+            if (isSmallPrime(Integer.bitCount(i)))
+                ans++;
+
+        return ans;
+    }
+
+    public static boolean isSmallPrime(int n) {
+        if (n == 2 || n == 3 || n == 5 || n == 7
+            || n == 11 || n == 13 || n == 17 || n == 19) return true;
+        return false;
     }
 
     public static int countPrimeSetBits(int L, int R) {
