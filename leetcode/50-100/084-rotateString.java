1cbbedb2980eb79b6501e4812df7f13eafe1843d
nurnisi <amir.nursultan@yandex.ru>
2018-05-01 14:55:38 +0600

    rotateString3

--- leetcode/50-100/084-rotateString.java
+++ leetcode/50-100/084-rotateString.java
@@ -3,10 +3,14 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(rotateString("abcde", "cdeab"));
+        System.out.println(rotateString("abcde", "cdeba"));
     }
 
     public static boolean rotateString(String A, String B) {
+        return A.length() == B.length() && (A + A).contains(B);
+    }
+
+    public static boolean rotateString3(String A, String B) {
         if (A.length() != B.length()) return false;
         if (A.length() == 0) return true;
 
