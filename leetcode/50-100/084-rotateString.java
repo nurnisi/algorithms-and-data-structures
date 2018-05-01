619caf55fa95da2da457cb4bf94619339df53fad
nurnisi <amir.nursultan@yandex.ru>
2018-05-01 15:37:52 +0600

    rotateString4

--- leetcode/50-100/084-rotateString.java
+++ leetcode/50-100/084-rotateString.java
@@ -1,12 +1,46 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(rotateString("abcde", "cdeba"));
+        System.out.println(rotateString("abcde", "cdeab"));
     }
 
     public static boolean rotateString(String A, String B) {
+        if (A.equals(B)) return true;
+
+        int MOD = 1_000_000_007;
+        int P = 113;
+        int Pinv = BigInteger.valueOf(P).modInverse(BigInteger.valueOf(MOD)).intValue();
+
+        long hb = 0, power = 1;
+        for (char x : B.toCharArray()) {
+            hb = (hb + power * x) % MOD;
+            power = power * P % MOD;
+        }
+
+        long ha = 0; power = 1;
+        char[] ca = A.toCharArray();
+        for (char x : ca) {
+            ha = (ha + power * x) % MOD;
+            power = power * P % MOD;
+        }
+
+        for (int i = 0; i < ca.length; i++) {
+            char x = ca[i];
+            ha += power * x - x;
+            ha %= MOD;
+            ha *= Pinv;
+            ha %= MOD;
+            if (ha == hb && (A.substring(i + 1) + A.substring(0, i + 1)).equals(B))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean rotateString4(String A, String B) {
         return A.length() == B.length() && (A + A).contains(B);
     }
 
