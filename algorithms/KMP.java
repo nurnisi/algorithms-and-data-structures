9b8ecb6b8cf460c38d762149ef65b11955970851
nurnisi <amir.nursultan@yandex.ru>
2018-05-01 16:43:52 +0600

    KMP

---
+++ algorithms/KMP.java
@@ -0,0 +1,39 @@
+/*
+    Knuth–Morris–Pratt (KMP) 
+    Pattern Matching (Substring search)
+*/
+
+public static boolean KMP(String A, String B) {
+    char[] text = A.toCharArray();
+    char[] pattern = B.toCharArray();
+
+    int[] temp = computeTemporaryArray(pattern);
+    int i = 0, j = 0;
+    while (i < text.length && j < pattern.length) {
+        if (text[i] == pattern[j]) {
+            i++;
+            j++;
+        } else {
+            if (j != 0) j = temp[j - 1];
+            else i++;
+        }
+    }
+    if (j == pattern.length) return true;
+    return false;
+}
+
+public static int[] computeTemporaryArray(char[] pattern) {
+    int[] temp = new int[pattern.length];
+    int index = 0;
+    for (int i = 1; i < pattern.length;) {
+        if (pattern[i] == pattern[index])
+            temp[i++] = ++index;
+        else {
+            if (index != 0)
+                index = temp[index - 1];
+            else
+                temp[i++] = 0;
+        }
+    }
+    return temp;
+}
