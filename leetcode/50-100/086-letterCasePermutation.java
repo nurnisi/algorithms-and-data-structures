1882ac997be37e482001e3232e083c55c2793c72
nurnisi <amir.nursultan@yandex.ru>
2018-05-04 15:34:14 +0600

    letterCasePermutation2

--- leetcode/50-100/086-letterCasePermutation.java
+++ leetcode/50-100/086-letterCasePermutation.java
@@ -3,10 +3,23 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(letterCasePermutation("12345a"));
+        System.out.println(letterCasePermutation("a12b"));
     }
 
     public static List<String> letterCasePermutation(String S) {
+        List<String> list = new ArrayList<>(Arrays.asList(S));
+        for (int i = 0; i < S.length(); i++) {
+            for (int j = 0, size = list.size(); S.charAt(i) > '9' && j < size; j++) {
+                char[] ch = list.get(j).toCharArray();
+                ch[i] ^= 32;
+                list.add(String.valueOf(ch));
+            }
+        }
+        return list;
+    }
+
+    //my solution: recursive
+    public static List<String> letterCasePermutation2(String S) {
         char[] arr = S.toLowerCase().toCharArray();
         List<String> list = new ArrayList<>();
 
