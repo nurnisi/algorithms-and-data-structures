cb81946ce66f238fb17fcae48b93bdf8cce61053
nurnisi <amir.nursultan@yandex.ru>
2018-05-05 14:15:23 +0600

    maxAreaOfIsland DFS iterative

--- leetcode/50-100/087-maxAreaOfIsland.java
+++ leetcode/50-100/087-maxAreaOfIsland.java
@@ -8,11 +8,45 @@ public class leetcode {
                                                        {0,1,1,1,0,1}}));
     }
 
+    //leetcode solutin: DFS iterative
+    public static int maxAreaOfIsland(int[][] grid) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int[] dr = new int[]{-1,1,0,0};
+        int[] dc = new int[]{0,0,-1,1};
+
+        int ans = 0;
+        for (int r0 = 0; r0 < grid.length; r0++)
+            for (int c0 = 0; c0 < grid[0].length; c0++)
+                if (grid[r0][c0] == 1 && !visited[r0][c0]) {
+                    int shape = 0;
+                    Stack<int[]> stack = new Stack<>();
+                    stack.push(new int[]{r0,c0});
+                    visited[r0][c0] = true;
+                    while (!stack.isEmpty()) {
+                        int[] node = stack.pop();
+                        int r = node[0], c = node[1];
+                        shape++;
+                        for (int k = 0; k < 4; k++) {
+                            int nr = r + dr[k], nc = c + dc[k];
+                            if (nr >= 0 && nr < grid.length &&
+                                    nc >= 0 && nc < grid[0].length &&
+                                    grid[nr][nc] == 1 && !visited[nr][nc]) {
+                                stack.push(new int[]{nr,nc});
+                                visited[nr][nc] = true;
+                            }
+                        }
+                    }
+                    ans = Math.max(ans, shape);
+                }
+
+        return ans;
+    }
+
     //leetcode solution: more understandable DFS recursive
     static int[][] grid;
     static boolean[][] visited;
 
-    public static int maxAreaOfIsland(int[][] grid) {
+    public static int maxAreaOfIsland3(int[][] grid) {
         int max = 0;
         leetcode.grid = grid;
         visited = new boolean[grid.length][grid[0].length];
