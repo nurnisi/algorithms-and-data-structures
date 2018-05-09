37b01ad81849e02ab4297f5b51fa516e6c362427
nurnisi <amir.nursultan@yandex.ru>
2018-05-09 23:35:06 +0600

    largeGroupPositions2

--- leetcode/50-100/089-largeGroupPositions.java
+++ leetcode/50-100/089-largeGroupPositions.java
@@ -8,6 +8,16 @@ public class leetcode {
 
     public List<List<Integer>> largeGroupPositions(String S) {
         List<List<Integer>> res = new ArrayList<>();
+        for (int cur = 0, run = 0; run < S.length(); run++)
+            if (run == S.length() - 1 || S.charAt(cur) != S.charAt(run + 1)) {
+                if (run - cur >= 3) res.add(Arrays.asList(cur, run));
+                cur = run + 1;
+            }
+        return res;
+    }
+
+    public List<List<Integer>> largeGroupPositions2(String S) {
+        List<List<Integer>> res = new ArrayList<>();
         int cur = 0, run = 1;
         for (; run < S.length(); run++) {
             if (S.charAt(cur) != S.charAt(run)) {
