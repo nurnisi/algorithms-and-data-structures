fb258a30f222a5c2d2cc90419867d2e77ee8a1c9
nurnisi <amir.nursultan@yandex.ru>
2018-05-10 18:41:47 +0600

    countBinarySubstrings2

--- leetcode/50-100/090-countBinarySubstrings.java
+++ leetcode/50-100/090-countBinarySubstrings.java
@@ -3,10 +3,23 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(countBinarySubstrings("10101"));
+        System.out.println(countBinarySubstrings("00110011"));
     }
 
+    //without auxiliary space
     public static int countBinarySubstrings(String s) {
+        int cur = 1, prev = 0, ans = 0;
+        for (int i = 0; i < s.length(); i++, cur++)
+            if (i + 1 == s.length() || s.charAt(i) != s.charAt(i+1)) {
+                ans += Math.min(prev, cur);
+                prev = cur;
+                cur = 0;
+            }
+        return ans;
+    }
+
+    //with auxiliary space
+    public static int countBinarySubstrings2(String s) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0, cur = 1; i < s.length(); i++, cur++) {
             if (i + 1 == s.length() || s.charAt(i) != s.charAt(i + 1)) {
