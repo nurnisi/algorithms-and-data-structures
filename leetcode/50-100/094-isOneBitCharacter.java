d2fe4caf23cf921339a2b5098b33010797df6eed
nurnisi <amir.nursultan@yandex.ru>
2018-05-14 14:28:47 +0600

    isOneBitCharacter2

--- leetcode/50-100/094-isOneBitCharacter.java
+++ leetcode/50-100/094-isOneBitCharacter.java
@@ -7,6 +7,13 @@ public class leetcode {
     }
 
     public static boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1)
+            i += bits[i] + 1;
+        return i == bits.length - 1;
+    }
+
+    public static boolean isOneBitCharacter2(int[] bits) {
         int i = 0, len = bits.length;
         while (i <= len - 2) {
             if (bits[i] == 1) i += 2;
