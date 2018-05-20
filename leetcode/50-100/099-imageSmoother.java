42f67a4fbe268327d32d5f38ed2b8756195afc0b
nurnisi <amir.nursultan@yandex.ru>
2018-05-20 12:50:57 +0600

    imageSmoother2

--- leetcode/50-100/099-imageSmoother.java
+++ leetcode/50-100/099-imageSmoother.java
@@ -6,6 +6,26 @@ public class leetcode {
     }
 
     public static int[][] imageSmoother(int[][] M) {
+        int R = M.length, C = M[0].length;
+        int[][] ans = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int count = 0;
+                for (int r = i - 1; r <= i + 1; r++) {
+                    for (int c = j - 1; c <= j + 1; c++) {
+                        if (r >= 0 && r < R && c >= 0 && c < C) {
+                            ans[i][j] += M[r][c];
+                            count++;
+                        }
+                    }
+                }
+                ans[i][j] /= count;
+            }
+        }
+        return ans;
+    }
+
+    public static int[][] imageSmoother2(int[][] M) {
         int[][] ans = new int[M.length][M[0].length];
         int[][] dir = new int[][]{{0,0}, {-1,0}, {-1,-1}, {0,-1}, {1,-1}, {1,0}, {1,1}, {0,1}, {-1,1}};
         for (int i = 0; i < M.length; i++) {
