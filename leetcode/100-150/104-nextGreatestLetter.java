4f42f38380bffdcef96d2470468a7ddbb777ac21
nurnisi <amir.nursultan@yandex.ru>
2018-05-23 11:23:06 +0600

    nextGreatestLetter3

--- leetcode/100-150/104-nextGreatestLetter.java
+++ leetcode/100-150/104-nextGreatestLetter.java
@@ -7,6 +7,13 @@ public class leetcode {
     }
 
     public char nextGreatestLetter(char[] letters, char target) {
+        for (char ch : letters)
+            if (ch > target)
+                return ch;
+        return letters[0];
+    }
+
+    public char nextGreatestLetter3(char[] letters, char target) {
         boolean[] seen = new boolean[26];
         for (char ch : letters)
             seen[ch - 'a'] = true;
