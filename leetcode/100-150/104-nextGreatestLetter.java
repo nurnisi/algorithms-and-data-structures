4dd2bc1b2fe807fe92c2b68bb00b605b3f64ffb4
nurnisi <amir.nursultan@yandex.ru>
2018-05-23 11:33:34 +0600

    nextGreatestLetter4

--- leetcode/100-150/104-nextGreatestLetter.java
+++ leetcode/100-150/104-nextGreatestLetter.java
@@ -7,6 +7,16 @@ public class leetcode {
     }
 
     public char nextGreatestLetter(char[] letters, char target) {
+        int lo = 0, hi = letters.length;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (letters[mi] <= target) lo = mi + 1;
+            else hi = mi;
+        }
+        return letters[lo % letters.length];
+    }
+
+    public char nextGreatestLetter4(char[] letters, char target) {
         for (char ch : letters)
             if (ch > target)
                 return ch;
