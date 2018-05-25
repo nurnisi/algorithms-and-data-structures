1e8580f3e257c7e91224310318381af2312dc68b
nurnisi <amir.nursultan@yandex.ru>
2018-05-25 11:34:01 +0600

    levelOrderBottom2

--- leetcode/100-150/106-levelOrderBottom.java
+++ leetcode/100-150/106-levelOrderBottom.java
@@ -13,7 +13,30 @@ public class leetcode {
         TreeNode(int x) { val = x; }
     }
 
+    //little improvement
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(0, list);
+        }
+
+        return res;
+    }
+
+    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
 
