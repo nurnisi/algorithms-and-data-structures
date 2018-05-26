61ed03bb0fff1f61e059f13305425cefb23abbdc
nurnisi <amir.nursultan@yandex.ru>
2018-05-26 13:08:31 +0600

    findLengthOfLCIS2

--- leetcode/100-150/107-findLengthOfLCIS.java
+++ leetcode/100-150/107-findLengthOfLCIS.java
@@ -7,6 +7,15 @@ public class leetcode {
     }
 
     public static int findLengthOfLCIS(int[] nums) {
+        int max = 0, anchor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i - 1] >= nums[i]) anchor = i;
+            max = Math.max(max, i - anchor + 1);
+        }
+        return max;
+    }
+
+    public static int findLengthOfLCIS2(int[] nums) {
         if (nums.length == 0) return 0;
         int max = 1, count = 1;
         for (int i = 0; i < nums.length - 1; i++) {
