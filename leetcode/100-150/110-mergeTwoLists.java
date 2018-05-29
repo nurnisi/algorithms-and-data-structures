fdc9b1f3b7c0454778d15e484b95352a261ef19f
nurnisi <amir.nursultan@yandex.ru>
2018-05-29 12:07:03 +0600

    mergeTwoLists2

--- leetcode/100-150/110-mergeTwoLists.java
+++ leetcode/100-150/110-mergeTwoLists.java
@@ -18,7 +18,41 @@ public class leetcode {
         ListNode(int x) { val = x; }
     }
 
+    //my recursive solution
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        else if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    //error
+    public static ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        ListNode res = null, cur = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                if (res == null) res = new ListNode(l1.val);
+                else cur = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                if (res == null) res = new ListNode(l2.val);
+                else cur = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) cur.next = l1;
+        else cur.next = l2;
+        return res;
+    }
+
+    //my non-recursive long solution
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         else if (l2 == null) return l1;
         ListNode res;
