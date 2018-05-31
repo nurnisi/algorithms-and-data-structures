1fb59b61645a7fe306f3be9035a33ccef9f22bbc
nurnisi <amir.nursultan@yandex.ru>
2018-05-31 13:08:19 +0600

    longestWord2

--- leetcode/100-150/111-longestWord.java
+++ leetcode/100-150/111-longestWord.java
@@ -3,10 +3,65 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(longestWord(new String[]{"w","wo","wor","worl", "world"}));
+        System.out.println(longestWord2(new String[]{"w","wo","wor","worl", "world"}));
     }
 
-    public static String longestWord(String[] words) {
+    //trie solution
+    class Node {
+        char c;
+        Map<Character, Node> children = new HashMap<>();
+        int end;
+        public Node(char c) { this.c = c; }
+    }
+
+    class Trie {
+        Node root;
+        String[] words;
+        public Trie() { root = new Node('0'); }
+
+        public void insert(String word, int index) {
+            Node cur = root;
+            for (char c : word.toCharArray()) {
+                cur.children.putIfAbsent(c, new Node(c));
+                cur = cur.children.get(c);
+            }
+            cur.end = index;
+        }
+
+        public String dfs() {
+            String ans = "";
+            Stack<Node> stack = new Stack<>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                Node node = stack.pop();
+                if (node.end > 0 || node == root) {
+                    if (node != root) {
+                        String word = words[node.end - 1];
+                        if (word.length() > ans.length()
+                                || (word.length() == ans.length() && word.compareTo(ans) < 0)) {
+                            ans = word;
+                        }
+                    }
+                    for (Node n : node.children.values())
+                        stack.push(n);
+                }
+            }
+            return ans;
+        }
+    }
+
+    public String longestWord(String[] words) {
+        Trie trie = new Trie();
+        int index = 0;
+        for (String word : words) {
+            trie.insert(word, ++index);
+        }
+        trie.words = words;
+        return trie.dfs();
+    }
+
+    //my solution
+    public static String longestWord2(String[] words) {
         Arrays.sort(words);
         Set<String> set = new HashSet<>();
         set.add("");
