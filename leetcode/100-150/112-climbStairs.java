d77301ae1687350098354612c83336b808424a94
nurnisi <amir.nursultan@yandex.ru>
2018-06-01 18:33:27 +0600

    climbStairs2

--- leetcode/100-150/112-climbStairs.java
+++ leetcode/100-150/112-climbStairs.java
@@ -3,10 +3,17 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(6));
+        System.out.println(climbStairs(45));
     }
 
+    //recursive
     public static int climbStairs(int n) {
+        if (n <= 1) return 1;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    //iterative
+    public static int climbStairs2(int n) {
         if (n <= 3) return n;
         int ii = 2, jj = 3, cur = 4;
         while (cur <= n) {
