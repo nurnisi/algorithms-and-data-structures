e91324ad2afa28f5399d1589497f90e1853fea54
nurnisi <amir.nursultan@yandex.ru>
2018-06-01 18:36:39 +0600

    climbStairs3

--- leetcode/100-150/112-climbStairs.java
+++ leetcode/100-150/112-climbStairs.java
@@ -8,6 +8,17 @@ public class leetcode {
 
     //recursive
     public static int climbStairs(int n) {
+        return helper(0, n);
+    }
+
+    public static int helper(int i, int n) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+        return helper(i + 1, n) + helper(i + 2, n);
+    }
+
+    //recursive
+    public static int climbStairs3(int n) {
         if (n <= 1) return 1;
         return climbStairs(n - 1) + climbStairs(n - 2);
     }
