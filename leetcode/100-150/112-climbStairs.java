03e42ee19bbf4bcb08808ffd875dc57150178814
nurnisi <amir.nursultan@yandex.ru>
2018-06-02 21:02:01 +0600

    //recursion with memorization

--- leetcode/100-150/112-climbStairs.java
+++ leetcode/100-150/112-climbStairs.java
@@ -3,11 +3,24 @@ import java.util.*;
 public class leetcode {
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(45));
+        System.out.println(climbStairs(6));
+    }
+
+    //recursion with memorization
+    public static int climbStairs(int n) {
+        return helper(0, n, new int[n]);
+    }
+
+    public static int helper(int i, int n, int[] memo) {
+        if (i > n) return 0;
+        if (i == n) return 1;
+        if (memo[i] > 0) return memo[i];
+        memo[i] = helper(i + 1, n, memo) + helper(i + 2, n, memo);
+        return memo[i];
     }
 
     //recursive
-    public static int climbStairs(int n) {
+    public static int climbStairs4(int n) {
         return helper(0, n);
     }
 
