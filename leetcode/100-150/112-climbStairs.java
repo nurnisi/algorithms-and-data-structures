84d354183e35019d857dbd0fe03540bd2f83f2a5
nurnisi <amir.nursultan@yandex.ru>
2018-06-03 19:19:48 +0600

    climbStairs5

--- leetcode/100-150/112-climbStairs.java
+++ leetcode/100-150/112-climbStairs.java
@@ -6,8 +6,18 @@ public class leetcode {
         System.out.println(climbStairs(6));
     }
 
-    //recursion with memorization
+    //dp
     public static int climbStairs(int n) {
+        if (n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+
+    //recursion with memorization
+    public static int climbStairs5(int n) {
         return helper(0, n, new int[n]);
     }
 
