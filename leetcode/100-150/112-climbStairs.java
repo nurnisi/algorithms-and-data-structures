2340a07e7773f34ba8e7b5bfc1c370adbe42489d
nurnisi <amir.nursultan@yandex.ru>
2018-06-05 10:43:55 +0600

    climbStairs6

--- leetcode/100-150/112-climbStairs.java
+++ leetcode/100-150/112-climbStairs.java
@@ -6,8 +6,37 @@ public class leetcode {
         System.out.println(climbStairs(6));
     }
 
-    //dp
+    //Binets Method: uses matrix multiplication to obtain the n^​th Fibonacci Number
     public static int climbStairs(int n) {
+        int[][] q = {{1,1},{1,0}};
+        int[][] res = pow(q, n);
+        return res[0][0];
+    }
+
+    public static int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1,0},{0,1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    public static int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+            }
+        }
+        return c;
+    }
+
+    //dp
+    public static int climbStairs6(int n) {
         if (n == 1) return 1;
         int[] dp = new int[n + 1];
         dp[1] = 1;
