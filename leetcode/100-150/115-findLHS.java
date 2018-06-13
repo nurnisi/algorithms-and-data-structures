15fca367ad80233c6de3645190e3eac33512666a
nurnisi <amir.nursultan@yandex.ru>
2018-06-13 07:53:53 +0600

    findLHS5

--- leetcode/100-150/115-findLHS.java
+++ leetcode/100-150/115-findLHS.java
@@ -74,3 +74,14 @@
         }
         return res;
     }
+
+    //using map
+    public int findLHS5(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int res = 0;
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        for (int key : map.keySet())
+            if (map.containsKey(key + 1))
+                res = Math.max(res, map.get(key) + map.get(key + 1));
+        return res;
+    }
