b86a6e77498057e31245a8236300bb190a3b5547
nurnisi <amir.nursultan@yandex.ru>
2018-06-13 08:00:44 +0600

    findLHS6

--- leetcode/100-150/115-findLHS.java
+++ leetcode/100-150/115-findLHS.java
@@ -85,3 +85,15 @@
                 res = Math.max(res, map.get(key) + map.get(key + 1));
         return res;
     }
+
+    //using single loop
+    public int findLHS(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int res = 0;
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.containsKey(num - 1)) res = Math.max(res, map.get(num) + map.get(num - 1));
+            if (map.containsKey(num + 1)) res = Math.max(res, map.get(num) + map.get(num + 1));
+        }
+        return res;
+    }
