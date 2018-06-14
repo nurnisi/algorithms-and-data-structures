c080f74fcf1d799ec3706e4c919889415fa15d3a
nurnisi <amir.nursultan@yandex.ru>
2018-06-14 08:57:15 +0600

    maxDistToClosest3 python

--- leetcode/100-150/116-maxDistToClosest.py
+++ leetcode/100-150/116-maxDistToClosest.py
@@ -34,3 +34,18 @@
         for i in range(N): 
             if seats[i] == 0: ans = max(ans, min(left[i], right[i]))
         return ans
+
+    def maxDistToClosest3(self, seats):
+        N = len(seats)
+        left, right = [N] * N, [N] * N
+        
+        for i in range(N):
+            if seats[i] == 1: left[i] = 0
+            elif i > 0: left[i] = left[i - 1] + 1
+
+        for i in range(N-1, -1, -1):
+            if seats[i] == 1: right[i] = 0
+            elif i < N - 1: right[i] = right[i + 1] + 1
+        
+        return max(min(left[i], right[i])
+                for i, seat in enumerate(seats) if not seat)
