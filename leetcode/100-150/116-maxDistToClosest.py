ff335f35718d46e5257245c50638ff127a7621e6
nurnisi <amir.nursultan@yandex.ru>
2018-06-14 08:54:31 +0600

    maxDistToClosest2 python

--- leetcode/100-150/116-maxDistToClosest.py
+++ leetcode/100-150/116-maxDistToClosest.py
@@ -1,4 +1,3 @@
-class Solution:
     def maxDistToClosest(self, seats):
         left, right, l = 0, 0, len(seats)
         while (seats[left] != 1):
@@ -18,4 +17,20 @@ class Solution:
         :type seats: List[int]
         :rtype: int
         """
+
+    def maxDistToClosest2(self, seats):
+        N = len(seats)
+        left, right = [N] * N, [N] * N
         
+        for i in range(N):
+            if seats[i] == 1: left[i] = 0
+            elif i > 0: left[i] = left[i - 1] + 1
+
+        for i in range(N-1, -1, -1):
+            if seats[i] == 1: right[i] = 0
+            elif i < N - 1: right[i] = right[i + 1] + 1
+        
+        ans = 0
+        for i in range(N): 
+            if seats[i] == 0: ans = max(ans, min(left[i], right[i]))
+        return ans
