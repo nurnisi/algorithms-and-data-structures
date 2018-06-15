0340266141332c51b33f9d7bfe5ecea2bcd7b727
nurnisi <amir.nursultan@yandex.ru>
2018-06-15 08:48:18 +0600

    maxDistToClosest3 java

--- leetcode/100-150/116-maxDistToClosest.java
+++ leetcode/100-150/116-maxDistToClosest.java
@@ -38,3 +38,17 @@
                 ans = Math.max(ans, Math.min(left[i], right[i]));
         return ans;
     }
+
+    public int maxDistToClosest(int[] seats) {
+        int N = seats.length, prev = -1, future = 0, ans = 0;
+        for (int i = 0; i < N; i++) {
+            if (seats[i] == 1) prev = i;
+            else {
+                while (future < N && seats[future] == 0 || future < i) future++;
+                int left = prev == -1 ? N : i - prev;
+                int right = future == N ? N : future - i;
+                ans = Math.max(ans, Math.min(left, right));
+            }
+        }
+        return ans;
+    }
--- leetcode/100-150/116-maxDistToClosest.py
+++ leetcode/100-150/116-maxDistToClosest.py
@@ -1,51 +1,51 @@
-    def maxDistToClosest(self, seats):
-        left, right, l = 0, 0, len(seats)
-        while (seats[left] != 1):
-            left += 1
-        while (seats[l - 1 - right] != 1):
-            right += 1
+def maxDistToClosest(self, seats):
+    left, right, l = 0, 0, len(seats)
+    while (seats[left] != 1):
+        left += 1
+    while (seats[l - 1 - right] != 1):
+        right += 1
 
-        res, arr, count = max(left, right), [0] * l, 0
-        for i in range(l):
-            if seats[i] == 1:
-                res = max(res, count/2)
-                count = 0
-            arr[i] = count
-            count += 1
-        return int(res)
-        """
-        :type seats: List[int]
-        :rtype: int
-        """
+    res, arr, count = max(left, right), [0] * l, 0
+    for i in range(l):
+        if seats[i] == 1:
+            res = max(res, count/2)
+            count = 0
+        arr[i] = count
+        count += 1
+    return int(res)
+    """
+    :type seats: List[int]
+    :rtype: int
+    """
 
-    def maxDistToClosest2(self, seats):
-        N = len(seats)
-        left, right = [N] * N, [N] * N
-        
-        for i in range(N):
-            if seats[i] == 1: left[i] = 0
-            elif i > 0: left[i] = left[i - 1] + 1
+def maxDistToClosest2(self, seats):
+    N = len(seats)
+    left, right = [N] * N, [N] * N
+    
+    for i in range(N):
+        if seats[i] == 1: left[i] = 0
+        elif i > 0: left[i] = left[i - 1] + 1
 
-        for i in range(N-1, -1, -1):
-            if seats[i] == 1: right[i] = 0
-            elif i < N - 1: right[i] = right[i + 1] + 1
-        
-        ans = 0
-        for i in range(N): 
-            if seats[i] == 0: ans = max(ans, min(left[i], right[i]))
-        return ans
+    for i in range(N-1, -1, -1):
+        if seats[i] == 1: right[i] = 0
+        elif i < N - 1: right[i] = right[i + 1] + 1
+    
+    ans = 0
+    for i in range(N): 
+        if seats[i] == 0: ans = max(ans, min(left[i], right[i]))
+    return ans
 
-    def maxDistToClosest3(self, seats):
-        N = len(seats)
-        left, right = [N] * N, [N] * N
-        
-        for i in range(N):
-            if seats[i] == 1: left[i] = 0
-            elif i > 0: left[i] = left[i - 1] + 1
+def maxDistToClosest3(self, seats):
+    N = len(seats)
+    left, right = [N] * N, [N] * N
+    
+    for i in range(N):
+        if seats[i] == 1: left[i] = 0
+        elif i > 0: left[i] = left[i - 1] + 1
 
-        for i in range(N-1, -1, -1):
-            if seats[i] == 1: right[i] = 0
-            elif i < N - 1: right[i] = right[i + 1] + 1
-        
-        return max(min(left[i], right[i])
-                for i, seat in enumerate(seats) if not seat)
+    for i in range(N-1, -1, -1):
+        if seats[i] == 1: right[i] = 0
+        elif i < N - 1: right[i] = right[i + 1] + 1
+    
+    return max(min(left[i], right[i])
+            for i, seat in enumerate(seats) if not seat)
