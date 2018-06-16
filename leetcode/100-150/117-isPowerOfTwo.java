a9f5a3d31783f5a750c065a98e9395acd450f054
nurnisi <amir.nursultan@yandex.ru>
2018-06-16 16:31:15 +0600

    isPowerOfTwo java

---
+++ leetcode/100-150/117-isPowerOfTwo.java
@@ -0,0 +1,8 @@
+    public boolean isPowerOfTwo(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) count++;
+            n >>= 1;
+        }
+        return count == 1;
+    }
