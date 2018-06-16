044cafd9b7b54953cf1930edb8fdb406e93eaef9
nurnisi <amir.nursultan@yandex.ru>
2018-06-16 17:25:40 +0600

    isPowerOfTwo3

--- leetcode/100-150/117-isPowerOfTwo.java
+++ leetcode/100-150/117-isPowerOfTwo.java
@@ -10,3 +10,8 @@
     public boolean isPowerOfTwo2(int n) {
         return n>0 && (n&(n-1))==0;
     }
+
+    public boolean isPowerOfTwo3(int n) {
+        return n>0 && Integer.bitCount()==1;
+    }
+
--- leetcode/100-150/117-isPowerOfTwo.py
+++ leetcode/100-150/117-isPowerOfTwo.py
@@ -1,5 +1,5 @@
 def isPowerOfTwo(self, n):
-    return n >= 0 and list(bin(n))[2:].count('1') == 1
+    return n >= 0 and bin(n).count('1') == 1
 
 def isPowerOfTwo2(self, n):
     return n>0 and (n&(n-1))==0
