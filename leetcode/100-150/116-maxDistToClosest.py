61e4fe07547e1ccabda74bd5b4af688a1e7e35a8
nurnisi <amir.nursultan@yandex.ru>
2018-06-16 15:36:29 +0600

    maxDistToClosest4 python

--- leetcode/100-150/116-maxDistToClosest.py
+++ leetcode/100-150/116-maxDistToClosest.py
@@ -64,3 +64,22 @@ def maxDistToClosest3(self, seats):
             ans = max(ans, min(left,right))
 
     return ans
+
+
+def maxDistToClosest(self, seats):
+    people = (i for i, seat in enumerate(seats) if seat)
+    prev, future = None, next(people)
+    
+    ans = 0
+    for i, seat in enumerate(seats):
+        if seat:
+            prev = i
+        else:
+            while future is not None and future < i:
+                future = next(people, None)
+            
+            left = float('inf') if prev is None else i - prev
+            right = float('inf') if future is None else future - i
+            ans = max(ans, min(left, right))
+            
+    return ans
