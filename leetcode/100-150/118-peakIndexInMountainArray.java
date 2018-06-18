d5f4171b1ce5c60b8c5a14f018030719b31878b5
nurnisi <amir.nursultan@yandex.ru>
2018-06-18 21:03:10 +0600

    peakIndexInMountainArray2 java

--- leetcode/100-150/118-peakIndexInMountainArray.java
+++ leetcode/100-150/118-peakIndexInMountainArray.java
@@ -3,3 +3,13 @@
         while (i < len-1 && A[i] < A[i+1]) i++;
         return i;
     }
+
+    public int peakIndexInMountainArray(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi-lo)/2;
+            if (A[mi] < A[mi+1]) lo = mi+1;
+            else hi = mi;
+        }
+        return lo;
+    }
