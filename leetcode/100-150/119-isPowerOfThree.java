cdb0235a291d83b8c6b3c3f7add72f9f99f569fb
nurnisi <amir.nursultan@yandex.ru>
2018-06-19 08:29:46 +0600

    isPowerOfThree3 java

--- leetcode/100-150/119-isPowerOfThree.java
+++ leetcode/100-150/119-isPowerOfThree.java
@@ -6,3 +6,7 @@
     public boolean isPowerOfThree2(int n) {
         return n>0 && (n==1 || (n%3==0 && isPowerOfThree(n/3)));
     }
+
+    public boolean isPowerOfThree3(int n) {
+        return Integer.toString(n, 3).matches("^10*$");
+    }
