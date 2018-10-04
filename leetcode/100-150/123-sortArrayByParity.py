3605b9bc598c2efd551d92744a0e8162365ef9be
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-04 13:27:18 -0700

    sortArrayByParity2

--- leetcode/100-150/123-sortArrayByParity.py
+++ leetcode/100-150/123-sortArrayByParity.py
@@ -1,19 +1,34 @@
-class Solution(object):
-    def sortArrayByParity(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        res = A[:]
-        i, j = 0, len(A) - 1
-        for num in A:
-            if num % 2 == 0:
-                res[i] = num
-                i+=1
-            else:
-                res[j] = num
-                j-=1
-            
-        return res
+def sortArrayByParity(self, A):
+    """
+    :type A: List[int]
+    :rtype: List[int]
+    """
+    res = A[:]
+    i, j = 0, len(A) - 1
+    for num in A:
+        if num % 2 == 0:
+            res[i] = num
+            i+=1
+        else:
+            res[j] = num
+            j-=1
+        
+    return res
 
+def sortArrayByParity2(A):
+    """
+    :type A: List[int]
+    :rtype: List[int]
+    """
+    i, j = 0, len(A) - 1
+    while i < j:
+        if A[i] % 2 == 1 and A[j] % 2 == 0:
+            A[i], A[j] = A[j], A[i]
+        if A[i] % 2 == 0:
+            i+=1
+        if A[j] % 2 == 1:
+            j-=1
+        
+    return A
 
+print(sortArrayByParity2([3,5,6,4]))
