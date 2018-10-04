d4c706f11aa8fb9d1b6810edb2dd217f7a144253
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-04 15:00:41 -0700

    projectionArea

---
+++ leetcode/100-150/124-projectionArea.py
@@ -0,0 +1,25 @@
+def projectionArea(grid):
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+    z = 0
+    for col in grid:
+        for row in col:
+            if row != 0: z += 1
+    
+    x = 0
+    for col in grid:
+        maxx = 0
+        for row in col:
+            maxx = max(row, maxx)
+        x += maxx
+
+    y = 0    
+    for i in range(len(grid)):
+        maxy = 0
+        for j in range(len(grid)):
+            maxy = max(grid[j][i], maxy)
+        y += maxy
+
+    return z + x + y
