036e5ca8b36ebdba3c016ef222b6c47845cfbbae
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-05 14:33:21 -0700

    smallestRangeI2

--- leetcode/100-150/126-smallestRangeI.py
+++ leetcode/100-150/126-smallestRangeI.py
@@ -10,5 +10,15 @@ def smallestRangeI(A, K):
     else: 
         return 0
 
+def smallestRangeI2(A, K):
+    """
+    :type A: List[int]
+    :type K: int
+    :rtype: int
+    """
+    if max(A) - min(A) - 2 * K > 0:
+        return max(A) - min(A) - 2 * K
+    else: 
+        return 0
 
-print(smallestRangeI(A = [1,3,6], K = 3))
+print(smallestRangeI2(A = [1,3,6], K = 3))
