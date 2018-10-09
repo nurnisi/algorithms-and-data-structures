d92d86ed4b0ae81474334599e57d5ad44771555c
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-09 13:12:45 -0700

    884. Uncommon Words from Two Sentences 2

--- leetcode/100-150/133-884. Uncommon Words from Two Sentences.py
+++ leetcode/100-150/133-884. Uncommon Words from Two Sentences.py
@@ -19,3 +19,12 @@ class Solution:
                 list.append(key)
 
         return list
+
+    def uncommonFromSentences2(self, A, B):
+        count = {}
+        for word in A.split():
+            count[word] = count.get(word, 0) + 1
+        for word in B.split():
+            count[word] = count.get(word, 0) + 1
+
+        return [word for word in count if count[word] == 1]
