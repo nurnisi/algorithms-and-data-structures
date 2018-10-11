b2ad5dcc464d51d4c297eccf0b870508dabe16e6
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-11 11:56:50 -0700

    896. Monotonic Array

--- leetcode/100-150/136-896. Monotonic Array.py
+++ leetcode/100-150/136-896. Monotonic Array.py
@@ -16,3 +16,7 @@ class Solution(object):
                 if A[i] < A[i+1]:
                     return False
         return True
+
+    def isMonotonic2(self, A):
+        return (all(A[i] <= A[i+1] for i in range(len(A)-1)) or 
+                all(A[i] >= A[i+1] for i in range(len(A)-1)))
