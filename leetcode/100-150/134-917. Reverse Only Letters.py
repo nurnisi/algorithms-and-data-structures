b8665280f86a3b53752ec4d0d8958a600cd1f711
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-10 23:57:48 -0700

    917. Reverse Only Letters

---
+++ leetcode/100-150/134-917. Reverse Only Letters.py
@@ -0,0 +1,22 @@
+def reverseOnlyLetters(S):
+    def isLetter(c):
+        if (ord(c) >= 65 and ord(c) < 91) or (ord(c) >= 97 and ord(c) < 123):
+            return True
+        return False
+
+    A = list(S)
+    i, j = 0, len(A) - 1
+    while i < j:
+        if isLetter(A[i]) and isLetter(A[j]):
+            A[i], A[j] = A[j], A[i]
+            i = i + 1
+            j = j - 1
+        if not isLetter(A[i]):
+            i = i + 1
+        if not isLetter(A[j]):
+            j = j - 1
+    
+    return ''.join(A)
+            
+print(reverseOnlyLetters("a-bC-dEf-ghIj"))
+        
