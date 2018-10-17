6d561e1e94b293d1b0840b0b2254b745d24b6a82
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-17 11:40:30 -0700

    53. Maximum Subarray

---
+++ leetcode/100-150/142-53. Maximum Subarray.py
@@ -0,0 +1,37 @@
+# 53. Maximum Subarray
+# attempt 1: does not work
+def maxSubArray2(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if len(nums) == 0: return 0
+        
+    sums = []
+    sums.append(nums[0])
+    mini = maxi = 0
+    for i in range(1, len(nums)):
+        sums.append(sums[-1] + nums[i])
+        mini = i if sums[mini] > sums[-1] else mini
+        maxi = i if sums[maxi] < sums[-1] else maxi
+    
+    return max(nums[mini], nums[maxi]) if maxi <= mini else sums[maxi] - sums[mini]
+
+def maxSubArray(nums):
+    mini = maxi = curSum = nums[0]
+    for i in range(0, len(nums)):
+        curSum += nums[i]
+        maxi = max(maxi, nums[i], curSum - mini)
+        mini = min(curSum, mini)
+
+    return maxi
+
+
+
+print(maxSubArray([8]))
+print(maxSubArray([-2,-1]))
+print(maxSubArray([-1,-2]))
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(maxSubArray([-1,0]))
+print(maxSubArray([1,2]))
+print(maxSubArray([-1]))
