97bcf2454a10ff1a3709b7d7b53f00cc807b6d4b
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-20 10:42:45 -0700

    optimal_supply_schedule

---
+++ tcss_543_adv_algorithms/03_dp_optimal_supply_schedule.py
@@ -0,0 +1,23 @@
+def optimal_supply_schedule(supplies, r, c):
+    # find optimal schedule
+    dp = [0] * (len(supplies) + 1)
+    for i in range(len(supplies)):
+        j = i + 1
+        if j < 3:
+            dp[j] = dp[j - 1] + supplies[i] * r
+        else:
+            dp[j] = min(dp[j - 4] + c * 4, dp[j - 1] + supplies[i] * r)
+
+    #backtrack optimal schedule
+    sc = ['B' for _ in supplies]
+    i = len(supplies) - 1
+    while i >= 0:
+        if dp[i + 1] - supplies[i] == dp[i]:
+            sc[i] = 'A'
+            i -= 1
+        else:
+            i -= 4
+    print(dp)
+    print(sc)
+
+optimal_supply_schedule([11,9,9,12,12,12,12,9,9,11], 1, 9)
