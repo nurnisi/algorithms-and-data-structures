c24c3d253b1b7fb1a019737669139f19eee2777c
Amir Nursultan <amir.nursultan@yandex.ru>
2018-10-22 23:15:33 -0700

    191. Number of 1 Bits 2

--- leetcode/100-150/144-191. Number of 1 Bits.py
+++ leetcode/100-150/144-191. Number of 1 Bits.py
@@ -1,10 +1,18 @@
 # 191. Number of 1 Bits
-def hammingDistance(n):
+def hammingWeight(n):
     count = 0
     while n:
         count += (n & 1)
         n >>= 1
     return count
+
+def hammingWeight2(self, n):
+	count = 0
+	while n:
+		count += 1
+		n &= (n-1)
+	return count
+
 
 print(hammingDistance(8))
             
