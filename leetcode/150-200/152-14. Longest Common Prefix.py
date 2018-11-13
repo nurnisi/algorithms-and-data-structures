674e0c6cfa0b8709359c2ff410573ab03b9f59bf
Amir Nursultan <amir.nursultan@yandex.ru>
2018-11-12 23:45:13 -0800

    14. Longest Common Prefix 2

--- leetcode/150-200/152-14. Longest Common Prefix.py
+++ leetcode/150-200/152-14. Longest Common Prefix.py
@@ -18,3 +18,29 @@ class Solution(object):
             tail = i
         return cm[:tail]
 
+    # divide and conquer
+    def longestCommonPrefix2(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ""
+        return self.divideAndConquer(strs, 0, len(strs) - 1)
+        
+    def divideAndConquer(self, strs, left, right):
+        if right - left == 0:
+            return strs[left]
+        elif right - left == 1:
+            return self.check(strs[left], strs[right])
+        else:
+            mid = (right + left) // 2
+            leftRet = self.divideAndConquer(strs, left, mid)
+            rightRet = self.divideAndConquer(strs, mid + 1, right)
+            return self.check(leftRet, rightRet)
+
+    def check(self, str1, str2):
+        i, l1, l2 = 0, len(str1), len(str2)
+        while i < l1 and i < l2 and str1[i] == str2[i]:
+            i += 1
+        return str1[:i]
+
