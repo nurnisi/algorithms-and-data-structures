97e31a97102fb8d4ef9e5e1f9e561e6fefee582a
Amir Nursultan <amir.nursultan@yandex.ru>
2018-11-15 13:05:45 -0800

    155. Min Stack 2

--- leetcode/150-200/155-155. Min Stack.py
+++ leetcode/150-200/155-155. Min Stack.py
@@ -31,3 +31,36 @@ print(minStack.getMin())
 print(minStack.pop())
 print(minStack.getMin())
 
+# one stack solution
+class MinStack2(object):
+    def __init__(self):
+        self.stack = []
+        self.min = None
+
+    def push(self, x):
+        # only push the old minimum value when the current 
+        # minimum value changes after pushing the new value x
+        if self.min == None or x <= self.min:
+            self.stack.append(self.min)
+            self.min = x
+        self.stack.append(x)
+
+    def pop(self):
+        # if pop operation could result in the changing of the current minimum value, 
+        # pop twice and change the current minimum value to the last minimum value.
+        if self.stack.pop() == self.min: self.min = self.stack.pop()
+    
+    def top(self):
+        return self.stack[-1]
+
+    def getMin(self):
+        return self.min
+
+minStack = MinStack2()
+minStack.push(0)
+minStack.push(1)
+minStack.push(0)
+print(minStack.getMin())
+minStack.pop()
+print(minStack.getMin())
+
