2c50f54163cd977a5b938507207d0de31adea72f
Amir Nursultan <amir.nursultan@yandex.ru>
2018-11-20 22:37:55 -0800

    knapsack_backtracking

--- tcss_543_advanced_algorithms/02_dp_knapsack_project.py
+++ tcss_543_advanced_algorithms/02_dp_knapsack_project.py
@@ -3,21 +3,33 @@ def knapsack_bottom_up_dp(weights, values, W):
     n = len(weights)
     opt_vals = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
     
-    # traversing over optimal values
+    # generating possible optimal values
     for i in range(1, n + 1):
         for w in range(1, W + 1):
             wi = weights[i - 1]
-            # 
+            # when weight of the current item is greater than current weight
+            # take the previous optimal value from top
             if wi > w:
                 opt_vals[i][w] = opt_vals[i - 1][w]
+            # otherwise, take the maximum between:
+            # putting the current item or taking the previous selection
             else:
                 opt_vals[i][w] = max(values[i - 1] + opt_vals[i - 1][w - wi], opt_vals[i - 1][w])
 
-    for i in opt_vals: print(i)
+    # backtracking
+    opt_subset = [0 for i in range(n)]
+    i, w = n, W
+    while i > 0 and w > 0:
+        wi = weights[i - 1]
+        if opt_vals[i][w] == values[i - 1] + opt_vals[i - 1][w - wi]:
+            opt_subset[i - 1] = 1
+            w -= wi
+        i -= 1    
 
-    return opt_vals[-1][-1]
+    # for i in opt_vals: print(i)
+    return (opt_vals[-1][-1], opt_subset)
 
-knapsack_bottom_up_dp([2,2,3], [2,3,4], 6)
+print(knapsack_bottom_up_dp([2,2,3], [2,3,4], 6))
 
 import itertools
 def knapsack_brute_force(weights, values, W):
@@ -40,5 +52,9 @@ def knapsack_brute_force(weights, values, W):
             opt_subset = cmb
     
     return (max_tot_value, opt_subset)
-        
-knapsack_brute_force([2,2,3],[2,3,4], 6)
+
+import random
+# print(random.sample(range(200), 100))
+# print(random.sample(range(300), 100))
+print(knapsack_brute_force([2,2,3],[2,3,4], 6))
+print(knapsack_bottom_up_dp(random.sample(range(200), 100),random.sample(range(300), 100), 1000))
