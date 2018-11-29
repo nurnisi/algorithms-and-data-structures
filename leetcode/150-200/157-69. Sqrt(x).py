f9e6286f10622eafd106b6b8f3904b9cbe50ff94
Amir Nursultan <amir.nursultan@yandex.ru>
2018-11-29 15:27:55 -0800

    69. Sqrt(x) 2

--- leetcode/150-200/157-69. Sqrt(x).py
+++ leetcode/150-200/157-69. Sqrt(x).py
@@ -14,4 +14,21 @@ class Solution:
             root += 1
             
         return root if root * root == x else root - 1
+    
+    # binary search O(logn)
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        left, right = 1, x
+        while True:
+            mid = (right + left) // 2
+            sq1, sq2 = mid ** 2, (mid + 1) ** 2
+            if sq1 <= x and sq2 > x:
+                return mid
+            elif sq1 > x:
+                right = mid
+            else:
+                left = mid
         
