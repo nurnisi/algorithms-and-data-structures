cba5bf60ff9e7df02b03f638dab6644f9037c6f3
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-14 15:54:12 -0800

    46. Permutations 3

--- leetcode/150-200/166-46. Permutations.py
+++ leetcode/150-200/166-46. Permutations.py
@@ -1,6 +1,7 @@
 # 46. Permutations
 from collections import deque
 class Solution:
+    # my recursive solution
     def permute(self, nums):
         """
         :type nums: List[int]
@@ -18,6 +19,34 @@ class Solution:
             perm.append(queue.popleft())
             self.helper(queue, perm, n, ans)
             queue.append(perm.pop())
+
+    # shorter solution with ranges
+    def permute2(self, nums):
+        ans, n = [], len(nums)
+        self.helper2(nums, 0, ans, n)
+        return ans
+
+    # permute num[begin..end]
+    # invariant: num[0..begin-1] have been fixed/permuted
+    def helper2(self, nums, begin, ans, n):
+        if begin >= n:
+            ans.append(nums.copy())
+        else:
+            for i in range(begin, n):
+                nums[begin], nums[i] = nums[i], nums[begin]
+                self.helper2(nums, begin+1, ans, n)
+                nums[begin], nums[i] = nums[i], nums[begin]
+    
+    # iterative
+    def permute3(self, nums):
+        perms = [[]]
+        for n in nums:
+            newPerms = []
+            for p in perms:
+                for i in range(len(p)+1):
+                    newPerms.append(p[:i] + [n] + p[i:])
+            perms = newPerms
+        return perms
     
 sol = Solution()
-print(sol.permute([1,2,3]))
+print(sol.permute3([1,2,3]))
