f8132b269a2fa35bdad580cb28d11162c67350e4
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-13 16:29:26 -0800

    238. Product of Array Except Self

---
+++ leetcode/150-200/164-238. Product of Array Except Self.py
@@ -0,0 +1,20 @@
+# 238. Product of Array Except Self
+class Solution:
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        product, ans, zeros, check = 1, [0 for i in nums], 0, False
+        for i in nums: 
+            if i: product *= i
+            else: zeros += 1
+                
+        for i in range(len(nums)):
+            if nums[i]: 
+                if zeros: ans[i] = 0 
+                else: ans[i] = product//nums[i]
+            elif zeros < 2: ans[i] = product
+
+        return ans
+        
