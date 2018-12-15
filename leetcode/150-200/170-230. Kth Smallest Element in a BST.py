94a88457b4e85ef2f666364c8cf61bee36ca6862
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-15 12:48:00 -0800

    230. Kth Smallest Element in a BST 2

--- leetcode/150-200/170-230. Kth Smallest Element in a BST.py
+++ leetcode/150-200/170-230. Kth Smallest Element in a BST.py
@@ -1,12 +1,13 @@
 # 230. Kth Smallest Element in a BST
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
+    # inorder traversal: recursive
     def kthSmallest(self, root, k):
         """
         :type root: TreeNode
@@ -21,4 +22,26 @@ class Solution:
         
         helper(root)
         return arr[k-1]
-            
+    
+    # iterative
+    def kthSmallest2(self, root, k):
+        cur, stack, res= root, [], []
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            if not k: return cur.val
+            cur = cur.right
+
+sol = Solution()
+root = TreeNode(5)
+root.left = TreeNode(3)
+root.right = TreeNode(6)
+root.left.left = TreeNode(2)
+root.left.right = TreeNode(4)
+root.left.left.left = TreeNode(1)
+print(sol.kthSmallest2(root, 3))
+        
+
