15ea8bdca4ea7508c30a0ea53b6d016f050a9802
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-17 15:38:37 -0800

    62. Unique Paths 2

--- leetcode/150-200/176-62. Unique Paths.py
+++ leetcode/150-200/176-62. Unique Paths.py
@@ -15,4 +15,13 @@ class Solution:
                 grid[i][j] = grid[i-1][j] + grid[i][j-1]
         
         return grid[-1][-1]
+
+    def uniquePaths(self, m, n):
+        grid = [[1 for _ in range(n)] for _ in range(m)]
+            
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        
+        return grid[-1][-1]
         
