4572b9b9c1b610b82c2144cdbb65812fae8075f5
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-17 13:57:04 -0800

    15. 3Sum 2

--- leetcode/150-200/175-15. 3Sum.py
+++ leetcode/150-200/175-15. 3Sum.py
@@ -1,7 +1,7 @@
 # 15. 3Sum
 from collections import defaultdict
 class Solution:
-    # TLE at test # 311
+    # TLE at test # 311 out of 313
     def threeSum(self, nums):
         n = len(nums)
         dic = defaultdict(set)
@@ -25,7 +25,33 @@ class Solution:
                         ans.append(arr)
                         table.add(key)
         return ans
+
+    def threeSum2(self, nums):
+        ans, n = [], len(nums)
+        nums.sort()
+        for i in range(n-2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i + 1, n - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s > 0:
+                    r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                    
+        return ans
         
 sol = Solution()
-print(sol.threeSum([-1, 0, 1, 2, -1, -4]))
-print(sol.threeSum([0,0,0]))
+print(sol.threeSum2([-1, 0, 1, 2, -1, -4]))
+print(sol.threeSum2([0,0,0]))
