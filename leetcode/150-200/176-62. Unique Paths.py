57c0916727a6ef677367e7201e6ad0b040ae90bd
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-17 15:44:39 -0800

    62. Unique Paths 3

--- leetcode/150-200/176-62. Unique Paths.py
+++ leetcode/150-200/176-62. Unique Paths.py
@@ -16,7 +16,8 @@ class Solution:
         
         return grid[-1][-1]
 
-    def uniquePaths(self, m, n):
+    # short
+    def uniquePaths2(self, m, n):
         grid = [[1 for _ in range(n)] for _ in range(m)]
             
         for i in range(1, m):
@@ -24,4 +25,15 @@ class Solution:
                 grid[i][j] = grid[i-1][j] + grid[i][j-1]
         
         return grid[-1][-1]
+
+    # optimized space complexity
+    def uniquePaths3(self, m, n):
+        top = bottom = [1 for _ in range(n)]
+        for _ in range(1, m):
+            for j in range(1, n):
+                bottom[j] = bottom[j-1] + top[j]
+                top = bottom
+        
+        return top[-1]
+
         
