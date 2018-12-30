45649964d6a89e16881379d297afd3077c8ff7ac
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-30 15:03:26 -0800

    75. Sort Colors 3

--- leetcode/150-200/183-75. Sort Colors.py
+++ leetcode/150-200/183-75. Sort Colors.py
@@ -1,5 +1,6 @@
 # 75. Sort Colors
 class Solution:
+    # count and sort in two passes
     def sortColors(self, nums):
         """
         :type nums: List[int]
@@ -16,6 +17,7 @@ class Solution:
         
         print(nums)
 
+    # in-place: my solution
     def sortColors2(self, nums):
         zero, two, cur = 0, len(nums)-1, 0
         while zero < two and cur <= two:
@@ -28,13 +30,29 @@ class Solution:
 
         print(nums)
 
+    # in-place: leetcode solution
+    def sortColors3(self, nums):
+        zero, cur, two = 0, 0, len(nums)-1
+        while cur <= two:
+            if nums[cur] == 0:
+                nums[zero], nums[cur] = nums[cur], nums[zero]
+                zero += 1
+                cur += 1
+            elif nums[cur] == 1:
+                cur += 1
+            else:
+                nums[two], nums[cur] = nums[cur], nums[two]
+                two -= 1
+
+        print(nums)
+
 sol = Solution()
-sol.sortColors2([2,0,2,1,1,0])
-sol.sortColors2([0,0,1,1,1,2])
-sol.sortColors2([0,1,2,0,1,2,0,1])
-sol.sortColors2([1,0])
-sol.sortColors2([0,0])
-sol.sortColors2([0])
-sol.sortColors2([0,2])
-sol.sortColors2([0,2,0])
+sol.sortColors3([2,0,2,1,1,0])
+sol.sortColors3([0,0,1,1,1,2])
+sol.sortColors3([0,1,2,0,1,2,0,1])
+sol.sortColors3([1,0])
+sol.sortColors3([0,0])
+sol.sortColors3([0])
+sol.sortColors3([0,2])
+sol.sortColors3([0,2,0])
 
