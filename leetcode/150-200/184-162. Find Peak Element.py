b412506dda66559fcc2369153983ae1e382124ef
Amir Nursultan <amir.nursultan@yandex.ru>
2018-12-30 16:25:46 -0800

    162. Find Peak Element 2

--- leetcode/150-200/184-162. Find Peak Element.py
+++ leetcode/150-200/184-162. Find Peak Element.py
@@ -21,7 +21,21 @@ class Solution:
             if nums[i-1] < nums[i] > nums[i+1]:
                 return i
 
+    # binary search
+    def findPeakElement2(self, nums):
+        l, r = 1, len(nums)
+        nums = [float('-inf')] + nums + [float('-inf')]
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid-1] < nums[mid] and nums[mid] > nums[mid+1]: return mid-1
+            if nums[mid-1] < nums[mid] and nums[mid] < nums[mid+1]: l = mid + 1
+            else: r = mid
+
+        return l-1
+
 sol = Solution()
-print(sol.findPeakElement([1,2,1]))
-print(sol.findPeakElement([1,2,3,1]))
-print(sol.findPeakElement([1,2,1,3,5,6,4]))
+print(sol.findPeakElement2([1,2,1]))
+print(sol.findPeakElement2([1,2,3,1]))
+print(sol.findPeakElement2([1,2,1,3,5,6,4]))
+print(sol.findPeakElement2([1]))
+print(sol.findPeakElement2([1,2]))
