d028aa33b68baff84e00f82a74b3268a5656d4d4
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-01 19:46:53 -0800

    300. Longest Increasing Subsequence 2

--- leetcode/150-200/185-300. Longest Increasing Subsequence.py
+++ leetcode/150-200/185-300. Longest Increasing Subsequence.py
@@ -23,32 +23,19 @@ class Solution:
 
         return helper(-1) - 1
 
+    # dp
     def lengthOfLIS2(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        d = collections.defaultdict(int)
-        arr = [float('inf')] * len(nums)
-        def helper(cur):
+        if not nums: return 0
+        dp = [0] * len(nums)
+        for i in range(len(nums)):
             curMax = 0
-            for i in range(cur+1, len(nums)):
-                if cur == -1 or (nums[cur] <= arr[i] and nums[cur] < nums[i]):
-                    if i in d:
-                        curMax = max(curMax, d[i])
-                    else:
-                        imax = helper(i)
-                        d[i] = max(d[i], imax)
-                        curMax = max(curMax, d[i])
-                    if cur != -1:
-                        arr[i] = nums[cur]
-            
-            return 1 + curMax
-
-        return helper(-1) - 1
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    curMax = max(curMax, dp[j])
+            dp[i] = curMax + 1
+        
+        return max(dp)
 
 sol = Solution()
-print(sol.lengthOfLIS([10,9,2,5,3,7,101,18]))
-print(sol.lengthOfLIS([0,1,2,4,6,7,16,18]))
-            
-
+print(sol.lengthOfLIS2([10,9,2,5,3,7,101,18]))
+print(sol.lengthOfLIS2([0,1,2,4,6,7,16,18]))
