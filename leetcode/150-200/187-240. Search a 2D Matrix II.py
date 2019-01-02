c255b2fe2c6a1a3ec39f1b49d02188a2e19c4b2b
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-02 14:41:39 -0800

    240. Search a 2D Matrix II 2

--- leetcode/150-200/187-240. Search a 2D Matrix II.py
+++ leetcode/150-200/187-240. Search a 2D Matrix II.py
@@ -1,5 +1,6 @@
 # 240. Search a 2D Matrix II
 class Solution:
+    # binary search O(nlogn)
     def searchMatrix(self, matrix, target):
         """
         :type matrix: List[List[int]]
@@ -19,12 +20,25 @@ class Solution:
         
         return False
 
+    # O(m+n)
+    def searchMatrix2(self, matrix, target):
+        if not matrix: return False
+        row, col = 0, len(matrix[0])-1
+        while row < len(matrix) and col >= 0:
+            if target == matrix[row][col]:
+                return True
+            elif target < matrix[row][col]:
+                col -= 1
+            elif target > matrix[row][col]:
+                row += 1
+        return False
+
 sol = Solution()
-print(sol.searchMatrix([
+print(sol.searchMatrix2([
   [1,   4,  7, 11, 15],
   [2,   5,  8, 12, 19],
   [3,   6,  9, 16, 22],
   [10, 13, 14, 17, 24],
   [18, 21, 23, 26, 30]
-], 5))
-print(sol.searchMatrix([[-5]], -5))
+], 20))
+print(sol.searchMatrix2([[-5]], -5))
