aed4ced5750d98bb12471b1a96882adbebc41f35
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-02 18:02:39 -0800

    279. Perfect Squares

--- leetcode/150-200/188-279. Perfect Squares.py
+++ leetcode/150-200/188-279. Perfect Squares.py
@@ -1,21 +1,19 @@
 # 279. Perfect Squares
 from math import sqrt
 class Solution:
-    # TLE
-    def numSquares(self, n):
-        # generate all perfect squares up to n
-        ps = [i*i for i in range(1, int(sqrt(n))+1)]
-
-        # dp
+    # dp
+    def numSquares2(self, n):
         dp = [0] + [float('inf')]*(n)
         for i in range(1, n+1):
-            for j in ps:
-                if  j <= i:
-                    dp[i] = min(dp[i-j]+1, dp[i])
-        print(ps, dp)
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i-j*j]+1, dp[i])
+                j += 1
+
         return dp[n]
 
 sol = Solution()
+print(sol.numSquares2(12))
 print(sol.numSquares(8285))
 print(sol.numSquares(6730))
-print(sol.numSquares(1103))
+print(sol.numSquares2(1103))
