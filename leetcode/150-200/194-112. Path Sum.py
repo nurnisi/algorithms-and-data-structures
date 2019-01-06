8d8ef2ab6e63111f5e8e8e25f86bbf51364c48e5
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-05 18:22:56 -0800

    112. Path Sum

--- leetcode/150-200/194-112. Path Sum.py
+++ leetcode/150-200/194-112. Path Sum.py
@@ -7,7 +7,7 @@
 #         self.right = None
 
 class Solution:
-    def hasPathSum(self, root, sum):
+    def hasPathSum2(self, root, sum):
         """
         :type root: TreeNode
         :type sum: int
@@ -24,4 +24,9 @@ class Solution:
             return flag
                  
         return dfs(root)
-        
+
+    def hasPathSum(self, root, sum):
+        if not root: return False
+        if not root.left and not root.right and sum - root.val == 0:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
