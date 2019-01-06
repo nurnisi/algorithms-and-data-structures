64e4a52104e696a6d71983e3b30307d039230467
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-05 18:06:58 -0800

    67. Add Binary 2

--- leetcode/150-200/193-67. Add Binary.py
+++ leetcode/150-200/193-67. Add Binary.py
@@ -30,9 +30,21 @@ class Solution:
 
         if c: arr += [str(c)]
         arr.reverse()
-        return ''.join(arr)   
+        return ''.join(arr)
+
+    def addBinary2(self, a, b):
+        ans, i, c, lena, lenb = "", 0, 0, len(a), len(b)
+        while i < lena or i < lenb:
+            if i < lena: c += int(a[lena - i - 1])
+            if i < lenb: c += int(b[lenb - i - 1])
+            ans = str(c & 1) + ans
+            c >>= 1
+            i += 1
+        
+        if c: ans = str(c) + ans
+        return ans
 
 sol = Solution()
-print(sol.addBinary("1", "0"))
+print(sol.addBinary("11", "1"))
 
         
