e5f6c4889afef9310872f326ba0fcefc9ea0042b
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-07 15:41:32 -0800

    922. Sort Array By Parity II

--- leetcode/200-250/201-922. Sort Array By Parity II.py
+++ leetcode/200-250/201-922. Sort Array By Parity II.py
@@ -1,6 +1,6 @@
 # 922. Sort Array By Parity II
 class Solution:
-    def sortArrayByParityII(self, A):
+    def sortArrayByParityII2(self, A):
         ans = A.copy()
         even, odd = 0, 1
         for i in A:
@@ -12,4 +12,14 @@ class Solution:
                 odd += 2
         
         return ans
-        
+
+    def sortArrayByParityII(self, A):
+        even, odd, n = 0, 1, len(A)
+        while even < n and odd < n:
+            if A[even] % 2 == 1 and A[odd] % 2 == 0: A[even], A[odd] = A[odd], A[even]
+            if A[even] % 2 == 0: even += 2
+            if A[odd] % 2 == 1: odd += 2
+        return A
+
+sol = Solution()
+print(sol.sortArrayByParityII([4,2,5,7]))
