4e0e3464c17dc4907ad78554039979d2485c7195
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-06 19:39:20 -0800

    929. Unique Email Addresses 2

--- leetcode/150-200/197-929. Unique Email Addresses.py
+++ leetcode/150-200/197-929. Unique Email Addresses.py
@@ -1,10 +1,6 @@
 # 929. Unique Email Addresses
 class Solution:
     def numUniqueEmails(self, emails):
-        """
-        :type emails: List[str]
-        :rtype: int
-        """
         s = set()
         for e in emails:
             sp = e.split('@')
@@ -12,5 +8,19 @@ class Solution:
             s.add(fin)
         return len(s)
 
+    def numUniqueEmails2(self, emails):
+        seen = set()
+        for e in emails:
+            seen.add(''.join(e.split('@')[0].split('.')).split('+')[0] + '@' + e.split('@')[1])
+        return len(seen)
+
+    def numUniqueEmails3(self, emails):
+        seen = set()
+        for e in emails:
+            local, domain = e.split('@')
+            if '+' in local: local = local[:local.index('+')]
+            seen.add(local.replace('.', '') + '@' + domain)
+        return len(seen)
+
 sol = Solution()
-print(sol.numUniqueEmails(["test.emailalex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]))
+print(sol.numUniqueEmails2(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]))
