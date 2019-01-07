29efdf514bfe2a4e1c2e6bd9f7799cb7ddaa1f5f
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-07 10:18:51 -0800

    297. Serialize and Deserialize Binary Tree

---
+++ leetcode/150-200/195-297. Serialize and Deserialize Binary Tree.py
@@ -0,0 +1,56 @@
+# 297. Serialize and Deserialize Binary Tree
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+from collections import deque
+class Codec:
+    def serialize(self, root):
+        levels = self.dfs(root)
+        arr = []
+        queue = deque([root])
+        while queue and levels:
+            s = len(queue)
+            for i in range(s):
+                node = queue.popleft()
+                arr.append(str(node.val) if node else "null")
+                queue.append(node.left if node else None)
+                queue.append(node.right if node else None)
+            levels -= 1
+        return ','.join(arr)
+    
+    def dfs(self, root):
+        if not root: return 0
+        return 1 + max(self.dfs(root.left), self.dfs(root.right))
+
+    def deserialize(self, data):
+        if not data: return None
+        arr = data.split(',')
+        root = TreeNode(int(arr[0]))
+        queue = deque([root])
+        i = 1
+        while i < len(arr):
+            node = queue.popleft()
+            left = TreeNode(arr[i])
+            right = TreeNode(arr[i+1])
+            node.left = left
+            node.right = right
+            queue.append(left)
+            queue.append(right)
+            i += 2
+        return root
+            
+# Your Codec object will be instantiated and called as such:
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+
+codec = Codec()
+print(codec.serialize(codec.deserialize(codec.serialize(root))))
