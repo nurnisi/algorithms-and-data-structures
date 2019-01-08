cdb8c938f11544997b58ca7f9078e5cfb5fa6f82
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-08 15:19:27 -0800

    114. Flatten Binary Tree to Linked List

---
+++ leetcode/150-200/200-114. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,31 @@
+# 114. Flatten Binary Tree to Linked List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def flatten(self, root):
+        self.dfs(root)
+
+    def dfs(self, node):
+        if not node: return None
+        left = self.dfs(node.left)
+        right = self.dfs(node.right)
+        if not left and not right: 
+            return (node, node)
+        elif not right:
+            node.left = None
+            node.right = left[0]
+            return (node, left[1])
+        elif left and right:
+            node.left = None
+            node.right = left[0]
+            left[1].right = right[0]
+        return (node, right[1])
+
+
+
+        
