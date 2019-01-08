ec4ca1a4368711d49685b4f92b4e7dbc639c1bd9
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-07 16:35:43 -0800

    953. Verifying an Alien Dictionary

--- leetcode/200-250/202-953. Verifying an Alien Dictionary.py
+++ leetcode/200-250/202-953. Verifying an Alien Dictionary.py
@@ -1,6 +1,6 @@
 # 953. Verifying an Alien Dictionary
 class Solution:
-    def isAlienSorted(self, words, order):
+    def isAlienSorted2(self, words, order):
         """
         :type words: List[str]
         :type order: str
@@ -20,7 +20,22 @@ class Solution:
                 return False
 
         return True
-        
+
+    def isAlienSorted(self, words, order):
+        d = {c: i for i, c in enumerate(order)}
+        for i in range(len(words)-1):
+            w1, w2, flag = words[i], words[i+1], True
+            for j in range(min(len(w1), len(w2))):
+                if w1[j] != w2[j]:
+                    if d[w1[j]] > d[w2[j]]: return False
+                    flag = False
+                    break
+            if flag and len(w1) > len(w2): return False
+        return True
+
+
 sol = Solution()
+print(sol.isAlienSorted(words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"))
+print(sol.isAlienSorted(words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"))
 print(sol.isAlienSorted(words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"))
         
