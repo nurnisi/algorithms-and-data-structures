f94e306126ecfac6fbb26a39dfdcac83908f35bb
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-12 18:26:07 -0800

    704. Binary Search

--- leetcode/200-250/211-704. Binary Search.py
+++ leetcode/200-250/211-704. Binary Search.py
@@ -1,4 +1,5 @@
 # 704. Binary Search
+from bisect import bisect_left
 class Solution:
     # iterative
     def search2(self, nums, target):
@@ -11,13 +12,18 @@ class Solution:
         return -1
 
     # recursive
-    def search(self, nums, target):
+    def search3(self, nums, target):
         def helper(l, r):
             if l >= r: return -1
             mid = (l + r) // 2
             if nums[mid] == target: return mid
             return helper(mid + 1, r) if nums[mid] < target else helper(l, mid)
         return helper(0, len(nums))
+
+    # leetcode solution
+    def search(self, nums, target):
+        index = bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
 
 sol = Solution()
 print(sol.search(nums = [-1,0,3,5,9,12], target = 9))
