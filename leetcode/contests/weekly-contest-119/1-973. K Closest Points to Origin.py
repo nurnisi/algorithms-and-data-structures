21f1f7160f5d54a71f21a0b2f439003b166e4711
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-14 22:40:57 -0800

    973. K Closest Points to Origin

--- leetcode/contests/weekly-contest-119/1-973. K Closest Points to Origin.py
+++ leetcode/contests/weekly-contest-119/1-973. K Closest Points to Origin.py
@@ -1,6 +1,7 @@
+# 973. K Closest Points to Origin
 class Solution:
     # Accepted
-    def kClosest(self, points, K):
+    def kClosest2(self, points, K):
         dist = []
         for p in points:
             dist.append((p[0]**2 + p[1]**2, p))
@@ -9,6 +10,11 @@ class Solution:
         for d in dist[:K]:
             ans.append(d[1])
         return ans
+
+    # revisited
+    def kClosest(self, points, K):
+        points.sort(key = lambda P: P[0]**2 + P[1]**2)
+        return points[:K]
 
 sol = Solution()
 print(sol.kClosest(points = [[1,3],[-2,2]], K = 1))
