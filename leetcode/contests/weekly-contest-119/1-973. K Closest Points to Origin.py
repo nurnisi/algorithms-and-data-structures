e4cb2414dfa4b5929f8708af9c7423fa4f44e723
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-15 11:52:42 -0800

    973. K Closest Points to Origin 5

--- leetcode/contests/weekly-contest-119/1-973. K Closest Points to Origin.py
+++ leetcode/contests/weekly-contest-119/1-973. K Closest Points to Origin.py
@@ -47,29 +47,48 @@ class Solution:
         else: return self.helper(left, K)
 
     # quickSelect 3
-    def kClosest(self, points, K):
+    def kClosest6(self, points, K):
         dist = [(p[0]**2 + p[1]**2, p) for p in points]
         self.helper2(dist, 0, len(dist)-1, K)
         return [p[1] for p in dist[:K]]
 
     def helper2(self, dist, i, j, K):
         oi, oj, r = i, j, dist[random.randint(i, j)]
-        while i <= j:
+        while i < j:
             if dist[i][0] >= r[0] and dist[j][0] < r[0]:
                 dist[i], dist[j] = dist[j], dist[i]
             if dist[i][0] < r[0]: i += 1
             if dist[j][0] >= r[0]: j -= 1
         if i < K: self.helper2(dist, i, oj, K)
         elif i > K: self.helper2(dist, oi, i, K)
+            
+    # quickSelect 4: leetcode solution
+    def kClosest(self, points, K):
+        dist = lambda i: points[i][0]**2 + points[i][1]**2
+
+        def work(i, j, K):
+            if i >= j: return
+            oi, oj = i, j
+            pivot = dist(random.randint(i, j))
+            while i < j:
+                while i < j and dist(i) < pivot: i += 1
+                while i < j and dist(i) > pivot: j -= 1
+                points[i], points[j] = points[j], points[i]
+            
+            if K <= i - oi + 1: work(oi, i, K)
+            else: work(i, oj, K - (i  - oi + 1))
+        
+        work(0, len(points) - 1, K)
+        return points[:K]
         
 sol = Solution()
 print(sol.kClosest(points = [[1,3],[-2,2]], K = 1))
 print(sol.kClosest(points = [[3,3],[5,-1],[-2,4]], K = 2))
 print(sol.kClosest([[-2,10],[-4,-8],[10,7],[-4,-7]], 3))
 
-print(sol.kClosest2(points = [[1,3],[-2,2]], K = 1))
-print(sol.kClosest2(points = [[3,3],[5,-1],[-2,4]], K = 2))
-print(sol.kClosest2([[-2,10],[-4,-8],[10,7],[-4,-7]], 3))
+# print(sol.kClosest2(points = [[1,3],[-2,2]], K = 1))
+# print(sol.kClosest2(points = [[3,3],[5,-1],[-2,4]], K = 2))
+# print(sol.kClosest2([[-2,10],[-4,-8],[10,7],[-4,-7]], 3))
 
 # print(sol.kClosest3(points = [[1,3],[-2,2]], K = 1))
 # print(sol.kClosest3(points = [[3,3],[5,-1],[-2,4]], K = 2))
