baa101fa9ca236781749f20f9416f4eab645afb2
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-15 14:38:13 -0800

    215. Kth Largest Element in an Array

--- leetcode/150-200/178-215. Kth Largest Element in an Array.py
+++ leetcode/150-200/178-215. Kth Largest Element in an Array.py
@@ -1,8 +1,9 @@
 # 215. Kth Largest Element in an Array
 import heapq
+import random
 class Solution:
     # sorting
-    def findKthLargest(self, nums, k):
+    def findKthLargest2(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
@@ -14,7 +15,7 @@ class Solution:
         return arr[-k]
 
     # max heap
-    def findKthLargest2(self, nums, k):
+    def findKthLargest3(self, nums, k):
         arr = [-num for num in nums]
         heapq.heapify(arr)
         for _ in range(k-1):
@@ -22,8 +23,34 @@ class Solution:
         return -heapq.heappop(arr)
 
     # heap: Leetcode solution
-    def findKthLargest(self, nums, k):
+    def findKthLargest4(self, nums, k):
         return heapq.nlargest(k, nums)[-1]
 
+    # quick select
+    def findKthLargest(self, nums, k):
+        def partition(i, j, pivotIndex):
+            pivot = nums[pivotIndex]
+            nums[pivotIndex], nums[j] = nums[j], nums[pivotIndex]
+            storeIndex = i
+            for x in range(i, j):
+                if nums[x] < pivot:
+                    nums[x], nums[storeIndex] = nums[storeIndex], nums[x]
+                    storeIndex += 1
+            
+            nums[storeIndex], nums[j] = nums[j], nums[storeIndex]
+            return storeIndex
+        
+        def select(i, j, k_smallest):
+            if i == j: return nums[i]
+            pivotIndex = random.randint(i, j)
+            pivotIndex = partition(i, j, pivotIndex)
+
+            if k_smallest == pivotIndex: return nums[k_smallest]
+            elif k_smallest < pivotIndex: return select(i, pivotIndex - 1, k_smallest)
+            else: return select(pivotIndex + 1, j, k_smallest)
+
+        select(0, len(nums) - 1, len(nums) - k)
+        return nums[len(nums) - k]
+
 sol = Solution()
-print(sol.findKthLargest2([3,2,1,5,6,4,3], 2))
+print(sol.findKthLargest([3,2,1,5,6,4,3], 2))
