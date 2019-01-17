238778252c8e93d003df9a7aa9c2e6e3440cc235
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-17 11:46:15 -0800

    96. Unique Binary Search Trees

--- leetcode/200-250/214-96. Unique Binary Search Trees.py
+++ leetcode/200-250/214-96. Unique Binary Search Trees.py
@@ -1,6 +1,6 @@
 # 96. Unique Binary Search Trees
 class Solution:
-    def numTrees(self, n):
+    def numTrees2(self, n):
         d = {0: 1, 1: 1}
         def helper(left, right):
             ans = 0
@@ -9,14 +9,26 @@ class Solution:
                     d[i-left] = helper(left, i-1)
                 if right-i not in d: 
                     d[right-i] = helper(i+1, right)
-                ans += (d[i-left] * d[right-i])
-                    
+                ans += (d[i-left] * d[right-i])   
             return ans
         ans = helper(1, n)
         return ans
+
+    def numTrees3(self, n):
+        G = [0] * (n+1)
+        G[0] = G[1] = 1
+        for i in range(2, n+1):
+            for j in range(1, i+1):
+                G[i] += G[j-1] * G[i-j]
+        return G[n]
+
+    def numTrees(self, n):
+        C = 1
+        for i in range(0, n):
+            C = C * 2*(2*i+1)/(i+2)
+        return int(C)
                     
 sol = Solution()
-print(sol.numTrees(0))
 print(sol.numTrees(1))
 print(sol.numTrees(2))
 print(sol.numTrees(3))
