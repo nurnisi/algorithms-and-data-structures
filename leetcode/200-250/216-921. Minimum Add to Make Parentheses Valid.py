f1f34378f1dcbffe121a1aaf1709b2658e8914f9
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-18 11:37:21 -0800

    921. Minimum Add to Make Parentheses Valid

---
+++ leetcode/200-250/216-921. Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,14 @@
+# 921. Minimum Add to Make Parentheses Valid
+class Solution:
+    def minAddToMakeValid(self, S):
+        stack = []
+        for c in S:
+            if c == ')' and stack and stack[-1] == '(':
+                stack.pop()
+            else: stack.append(c)
+        return len(stack)
+
+sol = Solution()
+print(sol.minAddToMakeValid("((())))"))
+
+        
