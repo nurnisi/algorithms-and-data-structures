13d5c9e6c649c240c1dcd1fed15576398b8d7069
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-17 22:29:39 -0800

    perm google 2

--- interviews/005-google/08-perm.py
+++ interviews/005-google/08-perm.py
@@ -6,17 +6,30 @@ that input[i]...input[i+25] forms a permutation
 of all letters 'A', 'Z'.
 """
 
-def permutation(S):
+def permutation2(S):
     arr, i = [0]*26, 0
     while i < len(S):
         arr[ord(S[i]) - ord('A')] += 1
         if i >= 26: arr[ord(S[i-26]) - ord('A')] -= 1
-        if ''.join(map(str, arr)) == "11111111111111111111111111":
+        if ''.join(map(str, arr)) == ''.join(['1']*26):
             return i - 25
         i += 1
     return -1
 
-print(permutation("ABFAABCBCDEFGHIJKLMNOPQRSTUVWXYZAHFJF"))
-print(len("11111111111111111111111111"))
-print(len("BCDEFGHIJKLMNOPQRSTUVWXYZA"))
+def permutation(S):
+    arr, i, count = [0]*26, 0, 0
+    while i < len(S):
+        c1 = ord(S[i]) - ord('A')
+        if not arr[c1]: count += 1
+        arr[c1] += 1
+        if i >= 26:
+            c2 = ord(S[i-26]) - ord('A')
+            arr[c2] -= 1
+            if not arr[c2]: count -= 1
+        if count == 26:
+            return i - 25
+        i += 1
+    return -1
+
+print(permutation("BBCDEFGHIJKLMNOPQRSTUVWXYZAHFJF"))
 
