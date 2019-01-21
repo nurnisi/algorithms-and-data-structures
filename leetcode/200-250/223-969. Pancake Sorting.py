85d95a7b000e3fe41ed02a3fffb866761c07ba4b
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-21 11:28:46 -0800

    969. Pancake Sorting

--- leetcode/200-250/223-969. Pancake Sorting.py
+++ leetcode/200-250/223-969. Pancake Sorting.py
@@ -1,6 +1,6 @@
 # 969. Pancake Sorting
 class Solution:
-    def pancakeSort(self, A):
+    def pancakeSort2(self, A):
         ans, n = [], len(A)
         B = sorted(range(1, n+1), key=lambda i: -A[i-1])
         
@@ -12,6 +12,14 @@ class Solution:
             n -= 1
         
         return ans
+    
+    def pancakeSort(self, A):
+        res = []
+        for x in range(len(A), 1, -1):
+            i = A.index(x)
+            res.extend([i+1, x])
+            A = A[:i:-1] + A[:i]
+        return res
 
 sol = Solution()
-print(sol.pancakeSort([3,2,4,1]))
+print(sol.pancakeSort2([3,2,4,1]))
