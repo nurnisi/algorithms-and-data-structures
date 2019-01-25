6ee94a28370ed06384c84c2ecfc9fbab78eb1583
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-25 10:18:34 -0800

    951. Flip Equivalent Binary Trees

--- leetcode/200-250/228-951. Flip Equivalent Binary Trees.py
+++ leetcode/200-250/228-951. Flip Equivalent Binary Trees.py
@@ -1,11 +1,12 @@
 # 951. Flip Equivalent Binary Trees
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+import itertools
 class Solution:
     def flipEquiv2(self, root1, root2):
         if not root1 and not root2: return True
@@ -24,14 +25,44 @@ class Solution:
                     self.flipEquiv(root1.right, root2.left)
         return False
 
-    def flipEquiv(self, root1, root2):
+    # leetcode: recursive
+    def flipEquiv3(self, root1, root2):
         if not root1 and not root2: return True
         if not root1 or not root2 or root1.val != root2.val: return False
         
         return (self.flipEquiv(root1.left, root2.left) and 
                 self.flipEquiv(root1.right, root2.right) or
-                self.flipEquiv(root1.left,, root2.right) and
+                self.flipEquiv(root1.left, root2.right) and
                 self.flipEquiv(root1.right, root2.left))
+
+    # leetcode: canonical traversal
+    def flipEquiv(self, root1, root2):
+        def dfs(node):
+            if node:
+                yield node.val
+                L = node.left.val if node.left else -1
+                R = node.right.val if node.right else -1
+                if L < R:
+                    yield from dfs(node.left)
+                    yield from dfs(node.right)
+                else:
+                    yield from dfs(node.right)
+                    yield from dfs(node.left)
+                yield '#'
+        
+        return all(x == y for x, y in itertools.zip_longest(dfs(root1), dfs(root2)))
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(8)
+
+sol = Solution()
+sol.flipEquiv(root, root)
+
 
         
         
