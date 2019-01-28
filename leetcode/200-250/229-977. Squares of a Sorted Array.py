c99f998362e2db2574ab1a16ac28a15964158d26
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-28 11:26:01 -0800

    977. Squares of a Sorted Array

--- leetcode/200-250/229-977. Squares of a Sorted Array.py
+++ leetcode/200-250/229-977. Squares of a Sorted Array.py
@@ -1,7 +1,19 @@
 # 977. Squares of a Sorted Array
 class Solution:
-    def sortedSquares(self, A):
+    def sortedSquares2(self, A):
         return sorted([i ** 2 for i in A])
+
+    def sortedSquares(self, A):
+        l, r = 0, len(A) - 1
+        A, ans = [i ** 2 for i in A], [0] * len(A)
+        while l <= r:
+            if A[l] > A[r]:
+                ans[r - l] = A[l]
+                l += 1
+            else:
+                ans[r - l] = A[r]
+                r -= 1
+        return ans
 
 sol = Solution()
 print(sol.sortedSquares([-4,-1,0,3,10]))
