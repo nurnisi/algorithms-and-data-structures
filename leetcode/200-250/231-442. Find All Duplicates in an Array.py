4681270e9191f7c1494ba561565b9da066a59c7a
Amir Nursultan <amir.nursultan@yandex.ru>
2019-01-30 22:19:56 -0800

    442. Find All Duplicates in an Array 3

--- leetcode/200-250/231-442. Find All Duplicates in an Array.py
+++ leetcode/200-250/231-442. Find All Duplicates in an Array.py
@@ -4,7 +4,7 @@ class Solution:
     def findDuplicates2(self, nums):
         return [k for k, v in collections.Counter(nums).items() if v > 1]
 
-    def findDuplicates(self, nums):
+    def findDuplicates3(self, nums):
         ans = set()
         i = 0
         while i < len(nums):
@@ -15,6 +15,13 @@ class Solution:
                     break            
             i += 1
         return list(ans)
+
+    def findDuplicates(self, nums):
+        ans = []
+        for i in nums:
+            if nums[abs(i) - 1] < 0: ans.append(abs(i))
+            else: nums[abs(i) - 1] *= -1
+        return ans
 
 sol = Solution()
 print(sol.findDuplicates([4,3,2,7,8,2,3,1]))
