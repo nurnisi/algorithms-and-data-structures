cb82facef95a254210f72152d2f108762d4cf83b
Amir Nursultan <amir.nursultan@yandex.ru>
2019-02-02 22:51:47 -0800

    865. Smallest Subtree with all the Deepest Nodes

--- leetcode/200-250/238-865. Smallest Subtree with all the Deepest Nodes.py
+++ leetcode/200-250/238-865. Smallest Subtree with all the Deepest Nodes.py
@@ -6,8 +6,9 @@
 #         self.left = None
 #         self.right = None
 
+import collections
 class Solution:
-    def subtreeWithAllDeepest(self, root):
+    def subtreeWithAllDeepest2(self, root):
         def helper(root):
             if not root: return (None, 0)
             left, lc = helper(root.left)
@@ -16,4 +17,15 @@ class Solution:
             elif lc < rc: return (right, rc + 1)
             else: return (root, rc + 1)
         return helper(root)[0]
+
+    # with nametuple
+    def subtreeWithAllDeepest(self, root):
+        Result = collections.namedtuple("Result", "node dist")
+        def helper(root):
+            if not root: return Result(None, 0)
+            L, R = helper(root.left), helper(root.right)
+            if L.dist > R.dist: return Result(L.node, L.dist + 1)
+            if R.dist > L.dist: return Result(R.node, R.dist + 1)
+            return Result(root, L.dist + 1)
+        return helper(root).node
         
