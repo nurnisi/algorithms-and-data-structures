2b0c312ea4069196f1c20072a35335a4415a40bd
Amir Nursultan <amir.nursultan@yandex.ru>
2019-02-02 19:08:22 -0800

    961. N-Repeated Element in Size 2N Array

--- leetcode/200-250/236-961. N-Repeated Element in Size 2N Array.py
+++ leetcode/200-250/236-961. N-Repeated Element in Size 2N Array.py
@@ -1,9 +1,17 @@
 # 961. N-Repeated Element in Size 2N Array
 import collections
 class Solution:
-    def repeatedNTimes(self, A):
+    def repeatedNTimes2(self, A):
         for k, v in collections.Counter(A).items():
             if v > 1: return k
+
+    def repeatedNTimes(self, A):
+        s = set()
+        for i in A:
+            if i in s:
+                return i
+            s.add(i)
+
 
 sol = Solution()
 print(sol.repeatedNTimes([1,2,3,3]))
