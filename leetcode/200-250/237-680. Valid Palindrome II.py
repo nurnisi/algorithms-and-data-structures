7028341ccf06f2c89a9a9c6a0f11ca9cdc15faf9
Amir Nursultan <amir.nursultan@yandex.ru>
2019-02-02 21:47:13 -0800

    680. Valid Palindrome II 3

--- leetcode/200-250/237-680. Valid Palindrome II.py
+++ leetcode/200-250/237-680. Valid Palindrome II.py
@@ -29,7 +29,14 @@ class Solution:
                 return is_pali(k+1, j) or is_pali(k, j-1)
         return True
 
-
+    def validPalindrome(self, s):
+        i, j = 0, len(s)-1
+        while i < j:
+            if s[i] != s[j]:
+                one, two = s[i:j], s[i+1:j+1]
+                return one == one[::-1] or two == two[::-1]
+            i, j = i+1, j-1
+        return True
 
 sol = Solution()
 print(sol.validPalindrome("ebcbbececabbacecbbcbe"))
