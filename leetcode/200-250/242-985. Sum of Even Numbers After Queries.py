4965f774513709e3b0a4ce9c0909404dfc8156ee
Amir Nursultan <amir.nursultan@yandex.ru>
2019-02-03 21:53:56 -0800

    985. Sum of Even Numbers After Queries

--- leetcode/200-250/242-985. Sum of Even Numbers After Queries.py
+++ leetcode/200-250/242-985. Sum of Even Numbers After Queries.py
@@ -1,6 +1,6 @@
 # 985. Sum of Even Numbers After Queries
 class Solution:
-    def sumEvenAfterQueries(self, A: 'List[int]', queries: 'List[List[int]]') -> 'List[int]':
+    def sumEvenAfterQueries2(self, A: 'List[int]', queries: 'List[List[int]]') -> 'List[int]':
         evens = sum(x for x in A if not x % 2)
         out = []
         for q in queries:
@@ -11,6 +11,18 @@ class Solution:
                 else: evens += q[0]
             out.append(evens)
             A[q[1]] += q[0]
+        return out
+
+    def sumEvenAfterQueries(self, A, queries):
+        S = sum(x for x in A if x % 2 == 0)
+        out = []
+
+        for v, i in queries:
+            if A[i] % 2 == 0: S -= A[i]
+            A[i] += v
+            if A[i] % 2 == 0: S += A[i]
+            out.append(S)
+        
         return out
 
 sol = Solution()
