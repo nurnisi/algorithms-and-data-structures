70ff6776dd2d6e0a255df26b056606f0ff6059f7
Amir Nursultan <amir.nursultan@yandex.ru>
2019-02-11 22:05:01 -0800

    384. Shuffle an Array 2

--- leetcode/200-250/246-384. Shuffle an Array.py
+++ leetcode/200-250/246-384. Shuffle an Array.py
@@ -1,6 +1,6 @@
 # 384. Shuffle an Array
 import random
-class Solution:
+class Solution2:
 
     def __init__(self, nums: 'List[int]'):
         self.nums = nums
@@ -21,7 +21,30 @@ class Solution:
                 self.shuf[i] = num
         return self.shuf
 
+class Solution:
+
+    def __init__(self, nums: 'List[int]'):
+        self.nums = nums
+        self.original = list(nums)
+
+    def reset(self) -> 'List[int]':
+        self.nums = self.original
+        self.original = list(self.original)
+        return self.nums
+
+    def shuffle(self) -> 'List[int]':
+        aux = list(self.nums)
+        for i in range(len(self.nums)):
+            ri = random.randrange(len(aux))
+            self.nums[i] = aux.pop(ri)
+        
+        return self.nums
+
 # Your Solution object will be instantiated and called as such:
 obj = Solution([1,2,3])
+print(obj.reset())
+print(obj.shuffle())
+print(obj.reset())
+print(obj.shuffle())
 print(obj.reset())
 print(obj.shuffle())
