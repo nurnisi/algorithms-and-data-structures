fd197aac91dbdc0ad37d6cb2b7338c021a06ab40
Amir Nursultan <amir.nursultan@yandex.ru>
2019-05-26 12:16:21 -0700

    509. Fibonacci Number

--- leetcode/249-509. Fibonacci Number.py
+++ leetcode/249-509. Fibonacci Number.py
@@ -6,11 +6,15 @@ class Solution:
             F.append(F[i - 1] + F[i - 2])
         return F[N]
 
-    def fib(self, N: int) -> int:
+    def fib3(self, N: int) -> int:
         if N < 2: return N
         prev, cur = 0, 1
         for i in range(2, N + 1):
             prev, cur = cur, prev + cur
         return cur
+
+    def fib(self, N: int) -> int:
+        if N < 2: return N
+        return self.fib(N - 1) + self.fib(N - 2)    
 
 print(Solution().fib(7))
