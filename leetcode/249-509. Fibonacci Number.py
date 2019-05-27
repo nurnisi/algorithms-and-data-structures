2eb087192fb5c3964b69b8b9c82d7d98dfff424a
Amir Nursultan <amir.nursultan@yandex.ru>
2019-05-27 11:29:15 -0700

    509. Fibonacci Number 4

--- leetcode/249-509. Fibonacci Number.py
+++ leetcode/249-509. Fibonacci Number.py
@@ -13,8 +13,26 @@ class Solution:
             prev, cur = cur, prev + cur
         return cur
 
-    def fib(self, N: int) -> int:
+    def fib4(self, N: int) -> int:
         if N < 2: return N
         return self.fib(N - 1) + self.fib(N - 2)    
+
+    F = [0] * 31
+    def fib(self, N: int) -> int:
+        if N < 2: return N
+        prev = cur = 0
+        if self.F[N - 2]: prev = self.F[N - 2]
+        else:
+            prev = self.fib(N - 2)
+            self.F[N - 2] = prev
+        
+        if self.F[N - 1]: cur = self.F[N - 1]
+        else:
+            cur = self.fib(N - 1)
+            self.F[N - 1] = cur
+
+        return prev + cur
+        
+
 
 print(Solution().fib(7))
