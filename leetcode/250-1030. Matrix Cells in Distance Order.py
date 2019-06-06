d5fbe6f9bb15694bd13caba5974c740403f76881
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-06 12:57:13 -0700

    1030. Matrix Cells in Distance Order

---
+++ leetcode/250-1030. Matrix Cells in Distance Order.py
@@ -0,0 +1,27 @@
+# 1030. Matrix Cells in Distance Order
+from collections import deque
+
+class Solution:
+    def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int):
+        matrix = [[0] * C for i in range(R)]
+        matrix[r0][c0] = 1
+
+        queue = deque()
+        queue.append([r0, c0])
+
+        ans = []
+        dirs = ((0, 1), (1, 0), (0, -1), (-1, 0)
+        
+        while queue:
+            r1, c1 = queue.popleft()
+            ans.append([r1, c1])
+            
+            for r, c in dirs:
+                if 0 <= r1 + r < R and 0 <= c1 + c < C and not matrix[r1 + r][c1 + c]:
+                    matrix[r1 + r][c1 + c] = 1
+                    queue.append([r1 + r, c1 + c])
+            
+        return ans
+        
+print(Solution().allCellsDistOrder(2, 3, 1, 2))
+
