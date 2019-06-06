32f7011ffc38da0e43708f32fad3afe732370681
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-06 16:19:24 -0700

    1002. Find Common Characters 2

--- leetcode/251-1002. Find Common Characters.py
+++ leetcode/251-1002. Find Common Characters.py
@@ -2,7 +2,7 @@
 from collections import Counter
 
 class Solution:
-    def commonChars(self, A):
+    def commonChars2(self, A):
         cnt_tot = Counter(A[0])
         
         for s in A[1:]:
@@ -13,5 +13,11 @@ class Solution:
         for k, v in cnt_tot.items():
             ans += [k] * v
         return ans
+
+    def commonChars(self, A):
+        cnt_tot = Counter(A[0])
+        for s in A:
+            cnt_tot &= Counter(s)
+        return list(cnt_tot.elements())
 
 print(Solution().commonChars(["bella","label","roller"]))
