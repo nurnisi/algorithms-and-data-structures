553ee3c58bd761bd25e9dc9d289ff91b09c36c1a
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-07 10:43:17 -0700

    1047. Remove All Adjacent Duplicates In String

--- leetcode/252-1047. Remove All Adjacent Duplicates In String.py
+++ leetcode/252-1047. Remove All Adjacent Duplicates In String.py
@@ -1,6 +1,6 @@
 # 1047. Remove All Adjacent Duplicates In String
 class Solution:
-    def removeDuplicates(self, S: str) -> str:
+    def removeDuplicates2(self, S: str) -> str:
         flag = True
         
         while flag:
@@ -17,5 +17,24 @@ class Solution:
             S = temp
                     
         return S
+
+    def removeDuplicates(self, S: str) -> str:
+        flag = True
+        arr = list(S)
+        
+        while flag:
+            flag = False
+            temp = []
+            i = 0
+            while i < len(arr):
+                if i == len(arr) - 1 or arr[i] != arr[i + 1]:
+                    temp.append(arr[i])
+                else:
+                    flag = True
+                    i += 1
+                i += 1
+            arr = temp
+                    
+        return ''.join(arr)
 
 print(Solution().removeDuplicates("abbaca"))
