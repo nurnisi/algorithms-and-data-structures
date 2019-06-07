e5d35efa4d8b6db41d883403d51b1a9bf65d1269
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-07 11:41:08 -0700

    1009. Complement of Base 10 Integer 2

--- leetcode/254-1009. Complement of Base 10 Integer.py
+++ leetcode/254-1009. Complement of Base 10 Integer.py
@@ -1,6 +1,16 @@
 # 1009. Complement of Base 10 Integer
 class Solution:
-    def bitwiseComplement(self, N: int) -> int:
+    def bitwiseComplement3(self, N: int) -> int:
         return int(''.join([str(int(x) ^ 1) for x in bin(N)[2:]]), 2)
+
+    def bitwiseComplement2(self, N: int) -> int:
+        X = 1
+        while N > X: X = X * 2 + 1
+        return X - N
+
+    def bitwiseComplement(self, N: int) -> int:
+        X = 1
+        while N > X: X = X * 2 + 1
+        return X ^ N
 
 print(Solution().bitwiseComplement(10))
