7894da8470602fd7dd27fc5b3987345d0ec790a6
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-21 07:43:05 -0700

    997. Find the Town Judge

--- leetcode/263-997. Find the Town Judge.py
+++ leetcode/263-997. Find the Town Judge.py
@@ -1,6 +1,6 @@
 # 997. Find the Town Judge
 class Solution:
-    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+    def findJudge2(self, N: int, trust: List[List[int]]) -> int:
         people = [[0, 0] for _ in range(N)]
         for a, b in trust:
             people[a - 1][0] += 1
@@ -9,5 +9,16 @@ class Solution:
         for i in range(N):
             if not people[i][0] and people[i][1] == N - 1:
                 return i + 1
+        
+        return -1
+
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        count = [0] * (N + 1)
+        for i, j in trust:
+            count[i] -= 1
+            count[j] += 1
+        for i in range(1, N + 1):
+            if count[i] == N - 1:
+                return i
         
         return -1
