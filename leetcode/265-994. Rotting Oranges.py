4b5e48d795693fb038b43d8e3f3b60da1eb20a8a
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-21 22:53:04 -0700

    863. All Nodes Distance K in Binary Tree

--- leetcode/265-994. Rotting Oranges.py
+++ leetcode/265-994. Rotting Oranges.py
@@ -3,28 +3,27 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid) -> int:
         rotten = deque([])
-        freshCnt = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        freshCnt, n, m = 0, len(grid), len(grid[0])
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == 2: rotten.append((i, j))
                 elif grid[i][j] == 1: freshCnt += 1
         
         if freshCnt == 0: return 0
         
-        freshCnt += len(rotten)
         time = -1
         dirs = ((0, 1), (1, 0), (0, -1), (-1, 0))
         while rotten:
             size = len(rotten)
             for _ in range(size):
                 r, c = rotten.popleft()
-                freshCnt -= 1
                 for rd, cd in dirs:
                     if 0 <= r + rd < len(grid) \
                         and 0 <= c + cd < len(grid[0]) \
                         and grid[r + rd][c + cd] == 1:
                         rotten.append((r + rd, c + cd))
                         grid[r + rd][c + cd] = 2
+                        freshCnt -= 1
             time += 1
             
         return time if freshCnt == 0 else -1
---
+++ leetcode/266-863. All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,81 @@
+# 863. All Nodes Distance K in Binary Tree
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def distanceK(self, root, target, K):
+        """
+        :type root: TreeNode
+        :type target: TreeNode
+        :type K: int
+        :rtype: List[int]
+        """
+        self.path = []
+        self.pathCopy = []
+        self.found = False
+        def findPath(node):
+            if self.found or not node: return
+            if node == target:
+                self.found = True
+                self.pathCopy = self.path.copy()
+                return
+            self.path.append(node)
+            findPath(node.left)
+            findPath(node.right)
+            self.path.pop()
+            
+        findPath(root)
+        self.pathCopy.append(target)
+        
+        ans = []
+        
+        def dfs(node, k):
+            if not node or k < 0: return
+            if k == 0:
+                ans.append(node.val)
+                return
+            dfs(node.left, k-1)
+            dfs(node.right, k-1)
+        
+        prev = None
+        while self.pathCopy:
+            cur = self.pathCopy.pop()
+            if cur.left and prev != cur.left:
+                dfs(cur.left, K - 1)
+            if cur.right and prev != cur.right:
+                dfs(cur.right, K - 1)
+            if K == 0:
+                ans.append(cur.val)
+            K -= 1
+            prev = cur
+            
+        return ans
+
+root = TreeNode(0)
+
+root.left = TreeNode(1)
+
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(2)
+
+print(Solution().distanceK(root, root.left.right, 1))
+
+# root = TreeNode(3)
+
+# root.left = TreeNode(5)
+# root.right = TreeNode(1)
+
+# root.left.left = TreeNode(6)
+# root.left.right = TreeNode(2)
+
+# root.right.left = TreeNode(0)
+# root.right.right = TreeNode(8)
+
+# root.left.right.left = TreeNode(7)
+# root.left.right.right = TreeNode(4)
+
+# print(Solution().distanceK(root, root.left, 2))
