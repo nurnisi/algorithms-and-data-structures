6fb751acdd00cd4162a6ff18e7ae0639f5bd753f
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-24 06:30:22 -0700

    916. Word Subsets 2

--- leetcode/278-916. Word Subsets.py
+++ leetcode/278-916. Word Subsets.py
@@ -1,7 +1,7 @@
 # 916. Word Subsets
 import collections
 class Solution:
-    def wordSubsets(self, A, B):
+    def wordSubsets2(self, A, B):
         CA = [collections.Counter(a) for a in A]
         for i in range(len(A)): CA[i]["word"] = A[i]
         
@@ -9,5 +9,23 @@ class Solution:
         for b in B: CB &= collections.Counter(b)
             
         return [w["word"] for w in list(filter(lambda a: all(a.get(k, 0) >= v for k, v in CB.items()), CA))]
+
+    def wordSubsets(self, A, B):
+        def count(w):
+            ans = [0] * 26
+            for c in w:
+                ans[ord(c) - ord('a')] += 1
+            return ans
+        
+        bmax = [0] * 26
+        for b in B:
+            for i, c in enumerate(count(b)):
+                bmax[i] = max(bmax[i], c)
+        
+        ans = []
+        for a in A:
+            if all(x >= y for x, y in zip(count(a), bmax)):
+                ans.append(a)
+        return ans
 
 print(Solution().wordSubsets(A = ["amazon","apple","facebook","google","leetcode"], B = ["e","o"]))
