63f6d87aae824605849aa31ddd155898c1b2f9b6
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-23 19:05:13 -0700

    647. Palindromic Substrings 2

--- leetcode/276-647. Palindromic Substrings.py
+++ leetcode/276-647. Palindromic Substrings.py
@@ -1,6 +1,6 @@
 # 647. Palindromic Substrings
 class Solution:
-    def countSubstrings(self, s: str) -> int:
+    def countSubstrings2(self, s: str) -> int:
         self.count = 0
         n = len(s)
 
@@ -14,5 +14,22 @@ class Solution:
             rec(i, i+1)
 
         return self.count
-    
+
+    def countSubstrings(self, s: str) -> int:
+        count, n = 0, len(s)
+        for k in range(n):
+            i = 0
+            while k - i >= 0 and k + i < n and s[k - i] == s[k + i]:
+                count += 1
+                i += 1
+                
+            i = 0
+            while k - i >= 0 and k + i + 1 < n and s[k - i] == s[k + i + 1]:
+                count += 1
+                i += 1
+        
+        return count
+            
+        
+print(Solution().countSubstrings2("abbaa"))    
 print(Solution().countSubstrings("abbaa"))
