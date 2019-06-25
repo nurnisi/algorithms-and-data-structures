3a26589340483cf7b90c865bf16b9c4a3cdb5c18
Amir Nursultan <amir.nursultan@yandex.ru>
2019-06-25 07:18:27 -0700

    712. Minimum ASCII Delete Sum for Two Strings 2

--- leetcode/280-712. Minimum ASCII Delete Sum for Two Strings.py
+++ leetcode/280-712. Minimum ASCII Delete Sum for Two Strings.py
@@ -1,6 +1,6 @@
 # 712. Minimum ASCII Delete Sum for Two Strings
 class Solution:
-    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+    def minimumDeleteSum2(self, s1: str, s2: str) -> int:
         dp = [[0] * (len(s2)+1) for _ in range(len(s1)+1)]
 
         for i in range(len(s1)-1, -1, -1):
@@ -17,5 +17,25 @@ class Solution:
                     dp[i][j] = min(dp[i+1][j] + ord(s1[i]), dp[i][j+1] + ord(s2[j]))
 
         return dp[0][0]
+
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        l1, l2 = len(s1)+1, len(s2)+1
+        dp = [[0] * l2 for _ in range(l1)]
+
+        for i in range(1, l1):
+            dp[i][0] = dp[i-1][0] + ord(s1[i-1])
+        for j in range(1, l2):
+            dp[0][j] = dp[0][j-1] + ord(s2[j-1])
+
+        for i in range(1, l1):
+            for j in range(1, l2):
+                if s1[i-1] == s2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j] + ord(s1[i-1]), dp[i][j-1] + ord(s2[j-1]))
+
+        return dp[i][j]    
+
+print(Solution().minimumDeleteSum("sea", "eat"))
 
 print(Solution().minimumDeleteSum("igijekdtywibepwonjbwykkqmrgmtybwhwjiqudxmnniskqjfbkpcxukrablqmwjndlhblxflgehddrvwfacarwkcpmcfqnajqfxyqwiugztocqzuikamtvmbjrypfqvzqiwooewpzcpwhdejmuahqtukistxgfafrymoaodtluaexucnndlnpeszdfsvfofdylcicrrevjggasrgdhwdgjwcchyanodmzmuqeupnpnsmdkcfszznklqjhjqaboikughrnxxggbfyjriuvdsusvmhiaszicfa", "ikhuivqorirphlzqgcruwirpewbjgrjtugwpnkbrdfufjsmgzzjespzdcdjcoioaqybciofdzbdieegetnogoibbwfielwungehetanktjqjrddkrnsxvdmehaeyrpzxrxkhlepdgpwhgpnaatkzbxbnopecfkxoekcdntjyrmmvppcxcgquhomcsltiqzqzmkloomvfayxhawlyqxnsbyskjtzxiyrsaobbnjpgzmetpqvscyycutdkpjpzfokvi"))
