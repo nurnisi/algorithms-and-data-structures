23f5b4852a2b09a2691fa1724e690b02d4ef151c
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-19 11:38:08 -0700

    581. Shortest Unsorted Continuous Subarray

--- leetcode/312-581. Shortest Unsorted Continuous Subarray.py
+++ leetcode/312-581. Shortest Unsorted Continuous Subarray.py
@@ -1,6 +1,6 @@
 # 581. Shortest Unsorted Continuous Subarray
 class Solution:
-    def findUnsortedSubarray(self, nums) -> int:
+    def findUnsortedSubarray2(self, nums) -> int:
         nums_sorted = sorted(nums)
         n = len(nums)
         i, j = -1, n
@@ -9,5 +9,28 @@ class Solution:
         while j > 0 and nums[j-1] == nums_sorted[j-1]:
             j -= 1
         return 0 if i == n-1 else j-i-1
+
+    def findUnsortedSubarray(self, nums) -> int:
+        l, r = 0, len(nums)-1
+        while l < r and nums[l] <= nums[l+1]: 
+            l += 1
+        
+        if l >= r: 
+            return 0
+        
+        while nums[r-1] <= nums[r]: 
+            r -= 1
+        
+        mini, maxi = min(nums[l:r+1]), max(nums[l:r+1])
+        
+        while l >= 0 and nums[l] > mini: 
+            l -= 1
+            
+        while r < len(nums) and nums[r] < maxi:
+            r += 1
+        
+        return r - l - 1
+        
+        
 
 print(Solution().findUnsortedSubarray([1,3,2,4,5]))
