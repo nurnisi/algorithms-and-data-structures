2c569f433bc93bd15db625c7f2dd8586959d0842
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-19 15:47:17 -0700

    875. Koko Eating Bananas

--- leetcode/315-875. Koko Eating Bananas.py
+++ leetcode/315-875. Koko Eating Bananas.py
@@ -1,19 +1,25 @@
 # 875. Koko Eating Bananas
 import math
 class Solution:
-    def minEatingSpeed(self, piles, H: int) -> int:
+    def minEatingSpeed2(self, piles, H: int) -> int:
         lo, hi = 1, max(piles)
         while lo < hi:
             mid = (lo + hi) // 2
             res = 0
             for p in piles:
-                if mid == 0: 
-                    print(lo, hi)
                 res += math.ceil(p / mid)
             if res <= H:
                 hi = mid
             else:
                 lo = mid + 1
+        return lo
+
+    def minEatingSpeed(self, piles: List[int], H: int) -> int:
+        lo, hi = 1, max(piles)
+        while lo < hi:
+            m = (lo + hi) // 2
+            if sum((p + m - 1) // m for p in piles) > H: lo = m + 1
+            else: hi = m
         return lo
 
 print(Solution().minEatingSpeed([312884470], 968709470))
