f8dc068956bfe880a972e2518103fbddc43c5244
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-20 16:34:06 -0700

    Longest Substring Without Repeating Characters

--- leetcode/explore/Yelp/02-Longest Substring Without Repeating Characters.py
+++ leetcode/explore/Yelp/02-Longest Substring Without Repeating Characters.py
@@ -1,6 +1,6 @@
 # Longest Substring Without Repeating Characters
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+    def lengthOfLongestSubstring2(self, s: str) -> int:
         mx = l = r = 0
         while r < len(s):
             if len(set(s[l:r+1])) == r-l+1:
@@ -9,5 +9,16 @@ class Solution:
             else:
                 l += 1
                 
+        return mx
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        mx = l = 0
+        st = set()
+        for c in s:
+            while st and c in st:
+                st.remove(s[l])
+                l += 1
+            st.add(c)
+            mx = max(mx, len(st))
         return mx
 
