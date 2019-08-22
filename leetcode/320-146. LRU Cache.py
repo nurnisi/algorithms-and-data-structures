1583e1eecce2c92de6f6ca5d5d3b5e9268cdeef2
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-21 19:21:54 -0700

    146. LRU Cache

--- leetcode/320-146. LRU Cache.py
+++ leetcode/320-146. LRU Cache.py
@@ -47,3 +47,65 @@ class LRUCache:
         elif len(self.d) == self.capacity:
             self.d.popitem(last=False)
         self.d[key] = value
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+class Node:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+    
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.d = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.d:
+            n = self.d[key]
+            self._remove(n)
+            self._add(n)
+            return n.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.d:
+            self._remove(self.d[key])
+        n = Node(key, value)
+        self._add(n)
+        self.d[key] = n
+        if len(self.d) > self.capacity:
+            n = self.head.next
+            self._remove(n)
+            del self.d[n.key]
+        
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.next = self.tail
+        node.prev = p
+        
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
