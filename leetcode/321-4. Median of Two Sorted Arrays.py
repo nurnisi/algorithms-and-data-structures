ddc013569ca80d3aceb77386b548626aee053648
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-23 13:56:06 -0700

    4. Median of Two Sorted Arrays

--- leetcode/321-4. Median of Two Sorted Arrays.py
+++ leetcode/321-4. Median of Two Sorted Arrays.py
@@ -1,6 +1,6 @@
 # 4. Median of Two Sorted Arrays
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays2(self, nums1, nums2) -> float:
         merge = []
         i = j = 0
         n1, n2 = len(nums1), len(nums2)
@@ -20,3 +20,31 @@ class Solution:
         if l % 2 == 0:
             return (merge[l//2-1] + merge[l//2]) / 2
         return merge[l//2]
+
+class Solution:
+    def findMedianSortedArrays(self, X: List[int], Y: List[int]) -> float:
+        if len(X) > len(Y): 
+            return self.findMedianSortedArrays(Y, X)
+        x, y = len(X), len(Y)
+        l, r = 0, x
+        
+        while True:
+            pX = (l + r) // 2
+            pY = (x + y + 1) // 2 - pX
+            
+            lX = X[pX - 1] if pX - 1 >= 0 else float('-inf')
+            rX = X[pX] if pX < x else float('inf')
+            lY = Y[pY - 1] if pY - 1 >= 0 else float('-inf')
+            rY = Y[pY] if pY < y else float('inf')
+            
+            if lX <= rY and lY <= rX:
+                break    
+            elif lX > rY:
+                r = pX
+            else:
+                l = pX + 1
+                
+                
+        if (x + y) % 2 == 0:
+            return (max(lX, lY) + min(rX, rY)) / 2
+        return max(lX, lY)
