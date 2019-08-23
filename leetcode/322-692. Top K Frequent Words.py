fad42551ec7ebcc4a823cd4f7ae35fac210df13c
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-23 12:22:43 -0700

    692. Top K Frequent Words

--- leetcode/322-692. Top K Frequent Words.py
+++ leetcode/322-692. Top K Frequent Words.py
@@ -15,3 +15,35 @@ class Solution:
                 break
         
         return ans
+
+class Element:
+    
+    def __init__(self, word, count):
+        self.word = word
+        self.count = count
+        
+    def __lt__(self, other):
+        if self.count == other.count:
+            return self.word > other.word
+        return self.count < other.count
+    
+    def __eq__(self, other):
+        return self.count == other.count and self.word == other.word
+
+class Solution:
+    
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        counts = collections.Counter(words)
+        
+        h = []
+        heapq.heapify(h)
+        for word, count in counts.items():
+            heapq.heappush(h, Element(word, count))
+            if len(h) > k:
+                heapq.heappop(h)
+        
+        ans = []
+        while h:
+            ans.append(heapq.heappop(h).word)
+        return ans[::-1]
+        
