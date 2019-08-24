9d8388814947f2dcd2769989639a595effa4f39a
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-23 19:06:14 -0700

    42. Trapping Rain Water

--- leetcode/323-42. Trapping Rain Water.py
+++ leetcode/323-42. Trapping Rain Water.py
@@ -1,7 +1,8 @@
 # 42. Trapping Rain Water
 # TLE
+import heapq
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap2(self, height) -> int:
         ans = 0
         bars = []
         
@@ -17,4 +18,24 @@ class Solution:
                     j += 1
         
         return ans
+
+    def trap(self, height) -> int:
+        ans = 0
+        l, r = 0, len(height)-1
+        lmax = rmax = 0
+        while l < r:
+            if height[l] < height[r]:
+                if height[l] >= lmax:
+                    lmax = height[l]
+                else:
+                    ans += lmax - height[l]
+                l += 1
+            else:
+                if height[r] >= rmax:
+                    rmax = height[r]
+                else:
+                    ans += rmax - height[r]
+                r -= 1
+        return ans
         
+print(Solution().trap([0,1,0,3,2,0,1,3,2,1,2,1]))
