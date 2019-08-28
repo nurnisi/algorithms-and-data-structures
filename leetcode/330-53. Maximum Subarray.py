b6cdb88cd0620c1f1ef6856d330b0e91fb7a4abf
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-28 16:48:14 -0700

    53. Maximum Subarray

--- leetcode/330-53. Maximum Subarray.py
+++ leetcode/330-53. Maximum Subarray.py
@@ -1,6 +1,6 @@
 # 53. Maximum Subarray
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray2(self, nums: List[int]) -> int:
         cum, ans, mn = 0, float('-inf'), 0
         for x in nums:
             cum += x
@@ -8,3 +8,10 @@ class Solution:
             mn = min(mn, cum)
         
         return ans
+
+    def maxSubArray(self, nums: List[int]) -> int:
+        cur_sum = max_sum = nums[0]
+        for x in nums[1:]:
+            cur_sum = max(x, cur_sum+x)
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
