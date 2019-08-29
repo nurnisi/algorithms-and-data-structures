eee3d3ab22a9165709724a9d936112e5fc6c44f4
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-28 18:50:51 -0700

    53. Maximum Subarray

--- leetcode/330-53. Maximum Subarray.py
+++ leetcode/330-53. Maximum Subarray.py
@@ -15,3 +15,29 @@ class Solution:
             cur_sum = max(x, cur_sum+x)
             max_sum = max(max_sum, cur_sum)
         return max_sum
+
+# Divide and conquer
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self.rec(nums, 0, len(nums)-1)
+    
+    def rec(self, nums, l, r):
+        if l == r: return nums[l]
+        mid = (l+r)//2
+        
+        return max(self.rec(nums, l, mid),
+                  self.rec(nums, mid+1, r),
+                  self.crossSum(nums, l, r, mid))
+
+    def crossSum(self, nums, l, r, mid):
+        lsum, lmax = 0, float('-inf')
+        for i in range(mid, l-1, -1):
+            lsum += nums[i]
+            lmax = max(lmax, lsum)
+            
+        rsum, rmax = 0, float('-inf')
+        for i in range(mid+1, r+1):
+            rsum += nums[i]
+            rmax = max(rmax, rsum)
+            
+        return lmax + rmax
