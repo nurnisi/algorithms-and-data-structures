bf169c1319bc3a9dfd5c3edaf3466b4874280001
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-29 19:17:59 -0700

    23. Merge k Sorted Lists

---
+++ leetcode/331-23. Merge k Sorted Lists.py
@@ -0,0 +1,63 @@
+# 23. Merge k Sorted Lists
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # recursive: TLE
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        while len(lists) > 1:
+            lists.append(self.helper(lists.pop(), lists.pop()))
+        return lists.pop() if lists else None
+        
+    def helper(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.helper(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.helper(l1, l2.next)
+            return l2
+
+    # iterative: TLE
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        while len(lists) > 1:
+            l1, l2 = lists.pop(), lists.pop()
+            dummy = cur = ListNode
+            while l1 and l2:
+                if l1.val < l2.val:
+                    cur.next = ListNode(l1.val)
+                    l1 = l1.next
+                else:
+                    cur.next = ListNode(l2.val)
+                    l2 = l2.next
+                cur = cur.next
+            cur.next = l1 or l2
+            lists.append(dummy.next)
+        return lists.pop() if lists else None
+
+    # divide and conquer
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        return self.helper(lists, 0, len(lists)-1) if lists else None
+        
+    def helper(self, lists, l, r):
+        if l == r: return lists[l]
+        
+        mid = (l+r)//2
+        l1 = self.helper(lists, l, mid)
+        l2 = self.helper(lists, mid+1, r)
+        
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                cur.next = ListNode(l2.val)
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
