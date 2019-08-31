3bf816d788a4b238b09fc156ff76f2dd911b4a9d
Amir Nursultan <amir.nursultan@yandex.ru>
2019-08-30 17:01:11 -0700

    56. Merge Intervals

--- leetcode/333-56. Merge Intervals.py
+++ leetcode/333-56. Merge Intervals.py
@@ -15,3 +15,12 @@ class Solution:
         
         ans.append([start, -heap[0]])
         return ans
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merged = []
+        for i in sorted(intervals):
+            if merged and merged[-1][1] >= i[0]:
+                merged[-1][1] = max(merged[-1][1], i[1])
+            else:
+                merged.append(i)
+        return merged
