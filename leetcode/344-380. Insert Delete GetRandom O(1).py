db03b8e35659879ca95082ff2e6ecb520f00ef70
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-04 14:37:11 -0700

    380. Insert Delete GetRandom O(1)

--- leetcode/344-380. Insert Delete GetRandom O(1).py
+++ leetcode/344-380. Insert Delete GetRandom O(1).py
@@ -3,36 +3,21 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.set = set()
         
-
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
         if val in self.set:
             return False
         self.set.add(val)
         return True
-        
 
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
         if val in self.set:
             self.set.remove(val)
             return True
         return False
-        
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
         return list(self.set)[random.randint(0, len(self.set)-1)]
         
 # Your RandomizedSet object will be instantiated and called as such:
@@ -40,3 +25,27 @@ class RandomizedSet:
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.nums, self.pos = [], {}        
+
+    def insert(self, val: int) -> bool:
+        if val not in self.pos:
+            self.nums.append(val)
+            self.pos[val] = len(self.nums)-1
+            return True
+        return False
+
+    def remove(self, val: int) -> bool:
+        if val in self.pos:
+            idx, last = self.pos[val], self.nums[-1]
+            self.nums[idx], self.pos[last] = last, idx
+            self.nums.pop()
+            self.pos.pop(val, 0)
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        return self.nums[random.randint(0, len(self.nums)-1)]
