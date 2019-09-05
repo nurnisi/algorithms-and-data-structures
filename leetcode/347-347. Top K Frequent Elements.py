56267128ad4ea6699450f9143167d3fc8f6d2621
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-05 11:50:27 -0700

    347. Top K Frequent Elements

--- leetcode/347-347. Top K Frequent Elements.py
+++ leetcode/347-347. Top K Frequent Elements.py
@@ -3,3 +3,26 @@ import collections
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         return [key for key, val in sorted(collections.Counter(nums).items(), key=lambda kv: kv[1], reverse=True)][:k]
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d = {}
+        counts = [set()]
+        
+        for x in nums:
+            if x not in d:
+                d[x] = 1
+                counts[d[x]-1].add(x)
+            else:
+                counts[d[x]-1].discard(x)
+                d[x] += 1
+                if d[x]-1 >= len(counts):
+                    counts.append(set())
+                counts[d[x]-1].add(x)
+        
+        ans = []
+        i = len(counts)-1
+        while i >= 0 and len(ans) < k:
+            ans.extend(counts[i])
+            i -= 1
+        
+        return ans
