fe464fadeb85f5dc5ca7760cf6b0d8b9e87101a1
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-05 12:36:30 -0700

    599. Minimum Index Sum of Two Lists

--- leetcode/347-599. Minimum Index Sum of Two Lists.py
+++ leetcode/347-599. Minimum Index Sum of Two Lists.py
@@ -15,3 +15,17 @@ class Solution:
             ans.append(heapq.heappop(heap)[1])
         
         return ans
+
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        d = {res: i for i, res in enumerate(list1)}
+        best, ans = float('inf'), []
+        
+        for j, res in enumerate(list2):
+            i = d.get(res, float('inf'))
+            if i + j < best:
+                best = i + j
+                ans = [res]
+            elif i + j == best:
+                ans.append(res)
+        
+        return ans
