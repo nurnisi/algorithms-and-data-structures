d41cb00dc0c4b9f08265dc7845be84297b3012cf
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-04 19:04:57 -0700

    355. Design Twitter

--- leetcode/346-355. Design Twitter.py
+++ leetcode/346-355. Design Twitter.py
@@ -24,6 +24,27 @@ class Twitter:
     def unfollow(self, followerId: int, followeeId: int) -> None:
         self.userFollows[followerId].discard(followeeId)
 
+import itertools
+import heapq
+class Twitter:
+    def __init__(self):
+        self.timer = itertools.count(step=-1)
+        self.tweets = collections.defaultdict(collections.deque)
+        self.followees = collections.defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].appendleft((next(self.timer), tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        tweets = heapq.merge(*(self.tweets[uid] for uid in self.followees[userId] | {userId}))
+        return [t for _, t in itertools.islice(tweets, 10)]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.followees[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followees[followerId].discard(followeeId)
+
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
 # obj.postTweet(userId,tweetId)
