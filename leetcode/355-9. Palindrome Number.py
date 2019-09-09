a80cf1b30aa5db25782a1f7567e02ab32fba8676
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-09 15:20:58 -0700

    9. Palindrome Number

--- leetcode/355-9. Palindrome Number.py
+++ leetcode/355-9. Palindrome Number.py
@@ -28,6 +28,17 @@ class Solution:
             y //= 100
             
         return True
+
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+    
+        revert = 0
+        while x > revert:
+            revert = revert * 10 + x % 10
+            x //= 10
+        
+        return x == revert or x == revert//10
             
 print(Solution().isPalindrome(121))
 print(Solution().isPalindrome(-121))
