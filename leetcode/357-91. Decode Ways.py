8ba204d899bb9336b09c6d4ec6d89cd552678ac9
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-13 17:13:02 -0700

    91. Decode Ways

--- leetcode/357-91. Decode Ways.py
+++ leetcode/357-91. Decode Ways.py
@@ -1,7 +1,6 @@
 # 91. Decode Ways
 class Solution:
     def numDecodings(self, s: str) -> int:
-        num = int(s)
         if s[0] == '0' or '00' in s: return 0
         
         dp = [1, 1]
@@ -17,3 +16,22 @@ class Solution:
                 dp.append(dp[-2]+dp[-1])
                 
         return dp[-1]
+
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0': return 0
+        
+        dp1 = dp2 = 1
+        for i in range(1, len(s)):
+            if s[i] == '0' and (s[i-1] == '0' or s[i-1] >= '3'): return 0
+            
+            if s[i] == '0':
+                dp1, dp2 = dp2, dp1
+            elif '10' <= s[i-1:i+1] <= '26':
+                dp1, dp2 = dp2, dp1+dp2
+            else:
+                dp1, dp2 = dp2, dp2
+        
+        return dp2
+            
+                
+            
