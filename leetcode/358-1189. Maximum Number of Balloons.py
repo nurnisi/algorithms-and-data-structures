f89a7e0e2c619c6bc7a6ccc03016c6ca2074c12e
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-14 23:29:34 -0700

    1189. Maximum Number of Balloons

---
+++ leetcode/358-1189. Maximum Number of Balloons.py
@@ -0,0 +1,13 @@
+# 1189. Maximum Number of Balloons
+import collections
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        cnt = collections.Counter(text)
+        ans = float('inf')
+        for ch in 'ban':
+            ans = min(ans, cnt.get(ch, 0))
+        
+        for ch in 'lo':
+            ans = min(ans, cnt.get(ch, 0) // 2)
+        
+        return ans
