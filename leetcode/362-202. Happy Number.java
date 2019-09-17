69fa42a0e775bb377eaf3cb72cb478b408e48246
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-16 23:00:31 -0700

    202. Happy Number

--- leetcode/362-202. Happy Number.java
+++ leetcode/362-202. Happy Number.java
@@ -17,3 +17,26 @@ class Solution {
         }
     }
 }
+
+class Solution {
+    int sqSum(int n) {
+        int sum = 0, tmp;
+        while (n != 0) {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = sqSum(slow);
+            fast = sqSum(fast);
+            fast = sqSum(fast);
+        } while (slow != fast);
+        if (slow == 1) return true;
+        return false;
+    }
+}
