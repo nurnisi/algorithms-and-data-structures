d749d9134010477a443cbcf95c0dbb75dc4aa4aa
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-21 13:47:53 -0700

    google practice

--- other/07-google-practice.py
+++ other/07-google-practice.py
@@ -6,5 +6,13 @@ def func(A, k):
     i = A.index(max_elem) # get it's index
     return A[i: k+i]
 
-A = [1, 4, 3, 2, 5]
-print(func(A, 4))
+def largest_subarray(A, k):
+    cum = [A[0]]
+    div = 10**(k-1)
+    for i in range(1, len(A)):
+        cum.append(cum[-1]%div*10 + A[i])
+    
+    return A[cum.index(max(cum))-k+1:][:k]
+
+A = [1, 4, 4, 4, 4, 4]
+print(largest_subarray(A, 4))
