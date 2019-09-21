e466c1ca87a92531c449d5d571546f738dbcf37e
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-21 16:58:59 -0700

    1007. Minimum Domino Rotations For Equal Row

--- leetcode/363-1007. Minimum Domino Rotations For Equal Row.py
+++ leetcode/363-1007. Minimum Domino Rotations For Equal Row.py
@@ -1,7 +1,7 @@
 # 1007. Minimum Domino Rotations For Equal Row
 import collections
 class Solution:
-    def minDominoRotations(self, A, B) -> int:
+    def minDominoRotations2(self, A, B) -> int:
         cnt_a = collections.Counter(A)
         cnt_b = collections.Counter(B)
         
@@ -24,6 +24,33 @@ class Solution:
                     ans = min(ans, ans_a, ans_b)
         
         return ans if ans != float('inf') else -1
+
+    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
+        swaps_a = [[i, 0] for i in range(1,7)]
+        swaps_b = [[i, 0] for i in range(1,7)]
+        
+        for i in range(len(A)):
+            if not swaps_a and not swaps_b:
+                return -1
+            
+            for j in range(len(swaps_a)-1, -1, -1):
+                if swaps_a[j][0] == A[i]:
+                    continue
+                elif swaps_a[j][0] == B[i]:
+                    swaps_a[j][1] += 1
+                else:
+                    swaps_a.pop(j)
+            
+            for j in range(len(swaps_b)-1, -1, -1):
+                if swaps_b[j][0] == B[i]:
+                    continue
+                elif swaps_b[j][0] == A[i]:
+                    swaps_b[j][1] += 1
+                else:
+                    swaps_b.pop(j)
+            
+                    
+        return min(sw for v, sw in swaps_a+swaps_b)
 
 print(Solution().minDominoRotations([6,1,6,4,6,6], [5,6,2,6,3,6]))
                 
