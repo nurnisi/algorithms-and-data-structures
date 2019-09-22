4e448f44da3fa566968ea9e2cba2ea4e94f6e4f5
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-22 15:31:58 -0700

    google practice

--- other/10-google-practice.py
+++ other/10-google-practice.py
@@ -1,5 +1,5 @@
 import collections, math
-def max_cake_area(radii, guests):
+def max_cake_area2(radii, guests):
     cnts = collections.Counter([x*x for x in radii])
     cnts_sorted = sorted(cnts.items(), key=lambda kv: kv[1], reverse=True)
 
@@ -16,13 +16,34 @@ def max_cake_area(radii, guests):
         return ar1 * math.pi
     return ar2 * math.pi * gst2 / i
 
+def max_cake_area(radii, guests):
+    areas = [math.pi*r*r for r in radii]
+
+    def possible(x):
+        k = 0
+        for a in areas:
+            k += a // x
+            if k >= guests:
+                return True
+        return False
+
+    l, r = 0, max(areas)
+    while l + 1e-5 <= r:
+        x = (l + r) / 2
+        if possible(x):
+            l = x
+        else:
+            r = x
+
+    return round(x, 4) 
+
 # radii = [1, 1, 1, 2, 2, 3]
 # numberOfGuests = 6
 
-# radii = [4, 3, 3]
-# numberOfGuests = 3
+radii = [4, 3, 3]
+numberOfGuests = 3
 
-radii = [6, 7]
-numberOfGuests = 12
+# radii = [6, 7]
+# numberOfGuests = 12
 
 print(max_cake_area(radii, numberOfGuests))
