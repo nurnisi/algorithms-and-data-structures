48213382c5d8b603031841f7348cf12fda1a77fb
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-22 12:39:02 -0700

    253. Meeting Rooms II

---
+++ leetcode/365-253. Meeting Rooms II.py
@@ -0,0 +1,10 @@
+# 253. Meeting Rooms II
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        ans, heap = 0, []
+        for s, e in sorted(intervals):
+            while heap and heap[0] <= s:
+                heapq.heappop(heap)
+            heapq.heappush(heap, e)
+            ans = max(ans, len(heap))
+        return ans
