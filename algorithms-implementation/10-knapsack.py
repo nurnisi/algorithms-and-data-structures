19410e78a0b0875c61ca4ab252352e25fc1f85f6
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-22 18:28:24 -0700

    Unbounded Knapsack

--- algorithms-implementation/10-knapsack.py
+++ algorithms-implementation/10-knapsack.py
@@ -1,4 +1,3 @@
-# Fractional Knapsack
 """
 Given weights and values of n items, we need to put these items
 in a knapsack of capacity W to get the maximum total value in the knapsack.
@@ -7,7 +6,7 @@ items = {{60, 10}, {100, 20}, {120, 30}} => (value, weight)
 W = 50;
 """
 
-# Greedy
+# Fractional Knapsack: Greedy
 def knapsack_fractional(items, W):
     cap = val = 0
     
@@ -24,7 +23,7 @@ capacity = 50
 
 print(knapsack_fractional(zip(val, wt), capacity))
 
-# 0-1 Knapsack
+# 0-1 Knapsack: DP
 def knapsack_0_1(wt, val, W):
     n = len(wt)
     dp = [[0] * (W+1) for _ in range(n+1)]
@@ -44,9 +43,27 @@ wt = [10, 20, 30]
 W = 50
 
 print(knapsack_0_1(wt, val, W))
-    
 
-            
+# Unbounded Knapsack: DP
+def knapsack_unbounded(wt, val, W):
+    n = len(wt)
+    dp = [0] * (W+1)
 
+    for w in range(1, W+1):
+        for i, cw in enumerate(wt):
+            if w >= cw:
+                dp[w] = max(dp[w], dp[w-cw] + val[i])
         
+    return dp[W]
+    
+W = 100
+val  = [1, 30]
+wt = [1, 50]
 
+print(knapsack_unbounded(wt, val, W))
+
+W = 8
+val = [10, 40, 50, 70]
+wt = [1, 3, 4, 5]
+
+print(knapsack_unbounded(wt, val, W))
