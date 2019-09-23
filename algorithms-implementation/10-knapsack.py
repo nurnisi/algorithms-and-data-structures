b04416ccafd26e362b882cd145d87e2c32771fa2
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-22 18:12:20 -0700

    0-1 Knapsack

--- algorithms-implementation/10-knapsack.py
+++ algorithms-implementation/10-knapsack.py
@@ -23,6 +23,27 @@ val = [60, 40, 100, 120]
 capacity = 50
 
 print(knapsack_fractional(zip(val, wt), capacity))
+
+# 0-1 Knapsack
+def knapsack_0_1(wt, val, W):
+    n = len(wt)
+    dp = [[0] * (W+1) for _ in range(n+1)]
+
+    for i in range(1, n+1):
+        w = wt[i-1]
+        for j in range(1, W+1):
+            if j - w >= 0:
+                dp[i][j] = max(val[i-1] + dp[i-1][j-w], dp[i-1][j])
+            else:
+                dp[i][j] = dp[i-1][j]
+    
+    return dp[n][W]
+
+val = [60, 100, 120] 
+wt = [10, 20, 30] 
+W = 50
+
+print(knapsack_0_1(wt, val, W))
     
 
             
