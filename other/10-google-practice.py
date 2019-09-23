48e8d446d04c219950e9605dd2a74e77e8687f38
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-22 18:49:07 -0700

    google practice

--- other/10-google-practice.py
+++ other/10-google-practice.py
@@ -35,15 +35,38 @@ def max_cake_area(radii, guests):
         else:
             r = x
 
-    return round(x, 4) 
+    return round(x, 4)
 
-# radii = [1, 1, 1, 2, 2, 3]
-# numberOfGuests = 6
+def getMaxCake(cakes, P):
+    def getArea(cake):
+        return math.pi * cake * cake
+    
+    N = len(cakes)
+    dp = []
+    for i in range(P + 1):
+        dp.append([0] * (N + 1))
+
+    for i in range(1, N + 1):
+        dp[1][i] = getArea(cakes[i - 1])
+
+    for p in range(2, P + 1):
+        for i in range(1, N + 1):
+            iarea = getArea(cakes[i - 1])
+            dp[p][i] = iarea / p
+            for j in range(1, P + 1):
+                dp[p][i] = max(min(dp[p - j][i - 1], iarea / j), 
+                                dp[p][i])
+
+    return round(dp[P][N], 4)
+
+radii = [1, 1, 1, 2, 2, 3]
+numberOfGuests = 6
+print(getMaxCake(radii, numberOfGuests))
 
 radii = [4, 3, 3]
 numberOfGuests = 3
+print(getMaxCake(radii, numberOfGuests))
 
-# radii = [6, 7]
-# numberOfGuests = 12
-
-print(max_cake_area(radii, numberOfGuests))
+radii = [6, 7]
+numberOfGuests = 12
+print(getMaxCake(radii, numberOfGuests))
