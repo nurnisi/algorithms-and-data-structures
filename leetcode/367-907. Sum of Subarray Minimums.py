c8db841678d924b78f83d1706a18455ab068522c
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-23 20:10:26 -0700

    907. Sum of Subarray Minimums

--- leetcode/367-907. Sum of Subarray Minimums.py
+++ leetcode/367-907. Sum of Subarray Minimums.py
@@ -24,7 +24,7 @@ class Solution:
         return ans % (10**9 + 7)
 
     # monotonous stack
-    def sumSubarrayMins(self, A) -> int:
+    def sumSubarrayMins4(self, A) -> int:
         # PLE: previous less element
         n = len(A)
         mon_stack, prev_less = [], [i for i in range(1,n+1)]
@@ -46,6 +46,22 @@ class Solution:
         for i in range(n):
             ans += A[i] * prev_less[i] * next_less[i]
         return ans % (10**9+7)
+
+    # monotonous stack
+    def sumSubarrayMins(self, A):
+        n, mod = len(A), 10**9 + 7
+        left, right, s1, s2 = [0] * n, [0] * n, [], []
+        for i in range(n):
+            count = 1
+            while s1 and s1[-1][0] > A[i]: count += s1.pop()[1]
+            left[i] = count
+            s1.append([A[i], count])
+        for i in range(n)[::-1]:
+            count = 1
+            while s2 and s2[-1][0] >= A[i]: count += s2.pop()[1]
+            right[i] = count
+            s2.append([A[i], count])
+        return sum(a * l * r for a, l, r in zip(A, left, right)) % mod
 
 A = [3,1,2,4]
 print(Solution().sumSubarrayMins(A))
