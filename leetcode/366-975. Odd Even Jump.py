baf377071be4fd8b124c60161ea237cb3b083895
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-23 17:33:46 -0700

    975. Odd Even Jump

--- leetcode/366-975. Odd Even Jump.py
+++ leetcode/366-975. Odd Even Jump.py
@@ -1,7 +1,8 @@
 # 975. Odd Even Jump
+import heapq
 class Solution:
-    # TLE
-    def oddEvenJumps(self, A):
+    # brute force: TLE
+    def oddEvenJumps2(self, A):
         n = len(A)
         jumps = [[False, False] for _ in A]
         jumps[n-1] = [True, True]
@@ -22,5 +23,39 @@ class Solution:
         
         return cnt
 
+    # min and max heaps: TLE
+    def oddEvenJumps3(self, A):
+        n = len(A)
+        jumps = [[False, False] for _ in A]
+        jumps[n-1] = [True, True]
+        minheap, maxheap = [(A[n-1], n-1)], []
+        
+        cnt = 1
+        for i in range(n-2, -1, -1):
+            while minheap and A[i] > minheap[0][0]:
+                v, idx = heapq.heappop(minheap)
+                heapq.heappush(maxheap, (-v, idx))
+            
+            while maxheap and A[i] <= -maxheap[0][0]:
+                v, idx = heapq.heappop(maxheap)
+                heapq.heappush(minheap, (-v, idx))
+            
+            if minheap:
+                jumps[i][0] = jumps[minheap[0][1]][1]
+
+            if minheap and minheap[0][0] == A[i]:
+                jumps[i][1] = jumps[minheap[0][1]][0]
+            elif maxheap:
+                jumps[i][1] = jumps[maxheap[0][1]][0]
+            
+            heapq.heappush(minheap, (A[i], i))
+            
+            cnt += 1 if jumps[i][0] else 0
+        
+        return cnt
+
 A = [10,13,12,14,15]
+A = [2,3,1,1,4]
+A = [5,1,3,4,2]
+A = [1,2,3,2,1,4,4,5]
 print(Solution().oddEvenJumps(A))
