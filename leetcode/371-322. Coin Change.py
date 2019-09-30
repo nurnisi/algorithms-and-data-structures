188aa5768e2a02fdcfe2feac5280cfba7605b1bc
Amir Nursultan <amir.nursultan@yandex.ru>
2019-09-30 11:59:08 -0700

    322. Coin Change

--- leetcode/371-322. Coin Change.py
+++ leetcode/371-322. Coin Change.py
@@ -1,6 +1,6 @@
 # 322. Coin Change
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
+    def coinChange2(self, coins: List[int], amount: int) -> int:
         dp = [[0]*(amount+1) for _ in range(len(coins)+1)]
         for i in range(amount+1): dp[0][i] = float('inf')
         
@@ -12,4 +12,15 @@ class Solution:
                     dp[i][j] = min(dp[i-1][j], dp[i][j-coins[i-1]] + 1)
                     
         return -1 if dp[-1][-1] == float('inf') else dp[-1][-1]
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')]*(amount+1)
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], dp[i-c]+1)
+                    
+        return -1 if dp[-1] == float('inf') else dp[-1]
                 
