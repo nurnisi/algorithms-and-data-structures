84f54e72c7eb381478e36ab1db87c8e771b2aa08
Amir Nursultan <amir.nursultan@yandex.ru>
2019-10-01 14:34:35 -0700

    127. Word Ladder

---
+++ leetcode/372-127. Word Ladder.py
@@ -0,0 +1,37 @@
+# 127. Word Ladder
+import collections
+class Solution:
+    # TLE
+    def ladderLength(self, beginWord: str, endWord: str, wordList) -> int:
+        if endWord not in wordList:
+            return 0
+        
+        self.ans = float('inf')
+        self.n = len(wordList)
+        
+        # create a dictionary
+        self.d = collections.defaultdict(list)
+        for kw in set([beginWord] + wordList):
+            for vw in wordList:
+                if sum(c1 != c2 for c1, c2 in zip(kw, vw)) == 1:
+                    self.d[kw].append(vw)        
+        
+        def dfs(depth, curWord, seen):
+            if depth > self.n:
+                return
+            if curWord == endWord:
+                self.ans = min(self.ans, depth)
+                return
+            
+            for w in self.d[curWord]:
+                if w not in seen:
+                    seen.add(w)
+                    dfs(depth+1, w, seen)
+                    seen.remove(w)
+        
+        dfs(1, beginWord, set([beginWord]))
+        return self.ans if self.ans != float('inf') else 0
+                
+print(Solution().ladderLength("a", "c", ["a","b","c"]))
+print(Solution().ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
+print(Solution().ladderLength("hot", "dog", ["hot","dog","dot"]))
