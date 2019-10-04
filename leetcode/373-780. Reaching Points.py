a3254f23126f58cd03e3b8af170bb59eedf1971a
Amir Nursultan <amir.nursultan@yandex.ru>
2019-10-04 14:38:19 -0700

    780. Reaching Points

--- leetcode/373-780. Reaching Points.py
+++ leetcode/373-780. Reaching Points.py
@@ -17,3 +17,9 @@ class Solution:
         
         return False
 
+    def reachingPoints(self, sx: int, sy: int, tx: int, ty: int) -> bool:
+        while sx < tx and sy < ty:
+            tx, ty = tx % ty, ty % tx
+        
+        return sx == tx and sy <= ty and (ty - sy) % sx == 0 or \
+                sy == ty and sx <= tx and (tx - sx) % sy == 0
