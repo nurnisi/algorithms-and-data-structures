0a1f84a3eb4c8f0b0a87873401c9858a6746becc
Amir Nursultan <amir.nursultan@yandex.ru>
2019-10-11 19:27:38 -0700

    twitter practice

--- other/19-twitter-practice.py
+++ other/19-twitter-practice.py
@@ -1,6 +1,7 @@
 import heapq
 
-def fountainActivation(ftns):
+# nlogn
+def fountainActivation2(ftns):
     ftRange = []
     n = len(ftns)
     for i, f in enumerate(ftns):
@@ -18,6 +19,24 @@ def fountainActivation(ftns):
             ans += 1
             prev = cur
             
+    return ans
+
+# n
+def fountainActivation(ftns):
+    n = len(ftns)
+    right_most = [0] * n
+    for i, x in enumerate(ftns):
+        l, r = max(i-x, 0), min(i+x+1, n)
+        right_most[l] = max(right_most[l], r)
+
+    ans = 1
+    r, next_r = right_most[0], 0
+    for i in range(n):
+        next_r = max(next_r, right_most[i])
+        if i == r:
+            ans += 1
+            r = next_r
+    
     return ans
 
 print(fountainActivation([1,2,1]))
