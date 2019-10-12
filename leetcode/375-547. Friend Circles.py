9577765b58b5fe2dcee943fc30aed9431dd8a784
Amir Nursultan <amir.nursultan@yandex.ru>
2019-10-12 13:09:54 -0700

    547. Friend Circles

--- leetcode/375-547. Friend Circles.py
+++ leetcode/375-547. Friend Circles.py
@@ -1,7 +1,8 @@
 # 547. Friend Circles
 import collections
 class Solution:
-    def findCircleNum(self, M: List[List[int]]) -> int:
+    # bfs
+    def findCircleNum2(self, M: List[List[int]]) -> int:
         visited = set()
         ans = 0
 
@@ -21,3 +22,31 @@ class Solution:
                         visited.add(j)
 
         return ans
+
+    # union find
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        n = len(M)
+        uf = UnionFind(n)
+        for i in range(n):
+            for j in range(n):
+                if M[i][j]:
+                    uf.union(i, j)
+
+        return len(uf.items)
+    
+class UnionFind:
+    def __init__(self, n):
+        self.items = [set([i]) for i in range(n)]
+
+    def find_index(self, item):
+        for i, x in enumerate(self.items):
+            if item in x:
+                return i
+
+    def union(self, a, b):
+        ai = self.find_index(a)
+        bi = self.find_index(b)
+
+        if ai != bi:
+            self.items[ai] |= self.items[bi]
+            self.items.pop(bi)
--- other/21-twitter-practice.py
+++ other/21-twitter-practice.py
@@ -1,5 +1,6 @@
 import collections
-def social_network(matrix):
+# BFS
+def social_network_bfs(matrix):
     visited = set()
     ans = 0
 
@@ -20,6 +21,34 @@ def social_network(matrix):
 
     return ans
 
+# union find
+class UnionFind:
+    def __init__(self, n):
+        self.items = [set([i]) for i in range(n)]
+
+    def find_index(self, item):
+        for i, x in enumerate(self.items):
+            if item in x:
+                return i
+
+    def union(self, a, b):
+        ai = self.find_index(a)
+        bi = self.find_index(b)
+
+        if ai != bi:
+            self.items[ai] |= self.items[bi]
+            self.items.pop(bi)
+
+def social_network_uf(matrix):
+    n = len(matrix)
+    uf = UnionFind(n)
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j]:
+                uf.union(i, j)
+
+    return len(uf.items)
+
 matrix = [
     [1,1,0,0],
     [1,1,1,0],
@@ -32,7 +61,7 @@ matrix = [
     [1,1,0],
     [0,0,1]
 ]
-print(social_network(matrix))
+print(social_network_uf(matrix))
 
 
                 
