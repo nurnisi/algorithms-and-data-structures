913de8d59cc6384ca8410104531f63420879fd33
Nursultan Amir <nursultan@Nursultans-MacBook-Pro-New.local>
2019-10-20 23:47:31 -0700

    139. Word Break

--- leetcode/380-139. Word Break.py
+++ leetcode/380-139. Word Break.py
@@ -1,24 +1,34 @@
 # 139. Word Break
 class Solution:
-    def wordBreak(self, s: str, wordDict) -> bool:
+    def wordBreak2(self, s: str, wordDict) -> bool:
         if not s or not wordDict:
             return False
         max_len = max(len(w) for w in wordDict)
         wordSet = set(wordDict)
         
-        queue = collections.deque()
+        queue = set()
         for i in range(1, max_len+1):
             if s[:i] in wordSet:
                 if s[i:] == "":
                     return True
-                queue.append(s[i:])
+                queue.add(s[i:])
         
         while queue:
-            cur = queue.popleft()
+            cur = queue.pop()
             for i in range(1, min(max_len, len(cur))+1):
                 if cur[:i] in wordSet:
                     if cur[i:] == "":
                         return True
-                    queue.append(cur[i:])
+                    queue.add(cur[i:])
                     
         return False
+
+    def wordBreak(self, s: str, wordDict) -> bool:
+        n = len(s)
+        dp = [True] + [False] * n
+        
+        for i in range(n):
+            for w in wordDict:
+                dp[i+1] = dp[i+1] or (w == s[i-len(w)+1:i+1] and dp[i-len(w)+1])
+        
+        return dp[-1]
